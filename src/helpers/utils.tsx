609a4dbbb449cbac1e2d05b8dee7fc797a0afa90
p-netm <pmuriuki98@gmail.com>
2019-07-03 18:34:39 +0300

    Remove unused imports

--- src/helpers/utils.tsx
+++ src/helpers/utils.tsx
@@ -1,27 +1,6 @@
 import { getOnadataUserInfo, getOpenSRPUserInfo } from '@onaio/gatekeeper';
 import { SessionState } from '@onaio/session-reducer';
-import { Color } from 'csstype';
-import { findKey, uniq } from 'lodash';
-import { FitBoundsOptions, Layer, Style } from 'mapbox-gl';
-import { Column } from 'react-table';
-import SeamlessImmutable from 'seamless-immutable';
-import { TASK_YELLOW } from '../colors';
-import { DIGITAL_GLOBE_CONNECT_ID, ONADATA_OAUTH_STATE, OPENSRP_OAUTH_STATE } from '../configs/env';
-import { locationHierarchy, LocationItem } from '../configs/settings';
-import {
-  BEDNET_DISTRIBUTION_CODE,
-  BLOOD_SCREENING_CODE,
-  CASE_CONFIRMATION_CODE,
-  FEATURE_COLLECTION,
-  IRS_CODE,
-  LARVAL_DIPPING_CODE,
-  MAP_ID,
-  MOSQUITO_COLLECTION_CODE,
-  RACD_REGISTER_FAMILY_CODE,
-} from '../constants';
-import { Plan } from '../store/ducks/plans';
-import { InitialTask } from '../store/ducks/tasks';
-import { colorMaps, ColorMapsTypes } from './structureColorMaps';
+import { ONADATA_OAUTH_STATE, OPENSRP_OAUTH_STATE } from '../configs/env';
 
 /** Interface for an object that is allowed to have any property */
 export interface FlexObject {
