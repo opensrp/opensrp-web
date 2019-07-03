a1096127c9b352faf16ee103921db5f55a781471
p-netm <pmuriuki98@gmail.com>
2019-07-03 14:21:01 +0300

    Remove color definitions for tasks

--- src/colors.tsx
+++ src/colors.tsx
@@ -11,18 +11,4 @@ export const BLACK = '#000';
 export type BLACK = typeof BLACK;
 export const GREY = '#666a6d';
 export type GREY = typeof GREY;
-
-
-// tasks
-export const TASK_PINK = '#FF96C2';
-export type TASK_PINK = typeof TASK_PINK;
-export const TASK_PURPLE = '#995EFF';
-export type TASK_PURPLE = typeof TASK_PURPLE;
-export const TASK_BLUE = '#3DA9DA';
-export type TASK_BLUE = typeof TASK_BLUE;
-export const TASK_GREEN = '#68BB0C';
-export type TASK_GREEN = typeof TASK_GREEN;
-export const TASK_RED = '#F12525';
-export type TASK_RED = typeof TASK_RED;
-export const TASK_YELLOW = '#FFCA16';
-export type TASK_YELLOW = typeof TASK_YELLOW;
+    
