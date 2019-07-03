50deec8e871aa505ffa1989cf7dc980484b8d652
p-netm <pmuriuki98@gmail.com>
2019-07-03 17:15:40 +0300

    Strip Home component off Reveal content

--- src/containers/pages/Home/Home.tsx
+++ src/containers/pages/Home/Home.tsx
@@ -1,9 +1,6 @@
 // this is the home page component
 import * as React from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Col, Row } from 'reactstrap';
-import { ENABLE_FI, ENABLE_IRS } from '../../../configs/env';
-import { FI_URL, IRS_URL } from '../../../constants';
+import { Col, Row } from 'reactstrap';
 import './Home.css';
 
 class Home extends React.Component<{}, {}> {
@@ -16,35 +13,8 @@ class Home extends React.Component<{}, {}> {
       <div className="text-center">
         <Row className="welcome-box">
           <Col>
-            <h3>Welcome to Reveal</h3>
-            <p>Get started by selecting an intervention below</p>
+            <h3>Welcome to OpenSRp</h3>
           </Col>
-        </Row>
-        <Row className="intervention-box">
-          {ENABLE_IRS && (
-            <Col md="6">
-              <Link to={IRS_URL} className="intervention-link">
-                <Button
-                  color="outline"
-                  className="btn-intervention btn-lg btn-block btn-outline-dark"
-                >
-                  IRS
-                </Button>
-              </Link>
-            </Col>
-          )}
-          {ENABLE_FI && (
-            <Col md="6">
-              <Link to={FI_URL} className="intervention-link">
-                <Button
-                  color="outline"
-                  className="btn-intervention btn-lg btn-block btn-outline-dark"
-                >
-                  Focus Investigation
-                </Button>
-              </Link>
-            </Col>
-          )}
         </Row>
       </div>
     );
