225b4ca53523b6773b8519d8089b40c1b658f105
p-netm <pmuriuki98@gmail.com>
2019-07-03 16:46:07 +0300

    Remove Reveal-specific env-derived consts

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,20 +1,12 @@
 /** This module handles settings taken from environment variables */
 
 /** The website name */
-export const WEBSITE_NAME = process.env.REACT_APP_WEBSITE_NAME || 'Reveal';
+export const WEBSITE_NAME = process.env.REACT_APP_WEBSITE_NAME || 'OpenSRP';
 export type WEBSITE_NAME = typeof WEBSITE_NAME;
 
 /** The domain name */
 export const DOMAIN_NAME = process.env.REACT_APP_DOMAIN_NAME || 'http://localhost:3000';
 export type DOMAIN_NAME = typeof DOMAIN_NAME;
-
-/** Do you want to enable the IRS features? */
-export const ENABLE_IRS = process.env.REACT_APP_ENABLE_IRS === 'true';
-export type ENABLE_IRS = typeof ENABLE_IRS;
-
-/** Do you want to enable the Focus Investigation features? */
-export const ENABLE_FI = process.env.REACT_APP_ENABLE_FI === 'true';
-export type ENABLE_FI = typeof ENABLE_FI;
 
 /** Do you want to enable the users page? */
 export const ENABLE_USERS = process.env.REACT_APP_ENABLE_USERS === 'true';
@@ -35,19 +27,6 @@ export type SUPERSET_API_BASE = typeof SUPERSET_API_BASE;
 /** The Superset API endpoint */
 export const SUPERSET_API_ENDPOINT = process.env.REACT_APP_SUPERSET_API_ENDPOINT || 'slice';
 export type SUPERSET_API_ENDPOINT = typeof SUPERSET_API_ENDPOINT;
-
-export const SUPERSET_PLANS_SLICE = process.env.REACT_APP_SUPERSET_PLANS_SLICE || '0';
-export type SUPERSET_PLANS_SLICE = typeof SUPERSET_PLANS_SLICE;
-
-export const SUPERSET_GOALS_SLICE = process.env.REACT_APP_SUPERSET_GOALS_SLICE || '0';
-export type SUPERSET_GOALS_SLICE = typeof SUPERSET_GOALS_SLICE;
-
-export const SUPERSET_JURISDICTIONS_SLICE =
-  process.env.REACT_APP_SUPERSET_JURISDICTIONS_SLICE || '0';
-export type SUPERSET_JURISDICTIONS_SLICE = typeof SUPERSET_JURISDICTIONS_SLICE;
-
-export const SUPERSET_STRUCTURES_SLICE = process.env.REACT_APP_SUPERSET_STRUCTURES_SLICE || '0';
-export type SUPERSET_STRUCTURES_SLICE = typeof SUPERSET_STRUCTURES_SLICE;
 
 /** OpenSRP oAuth2 settings */
 export const ENABLE_OPENSRP_OAUTH = process.env.REACT_APP_ENABLE_OPENSRP_OAUTH === 'true';
@@ -98,11 +77,5 @@ export type ONADATA_USER_URL = typeof ONADATA_USER_URL;
 export const ONADATA_OAUTH_STATE = process.env.REACT_APP_ONADATA_OAUTH_STATE || 'onadata';
 export type ONADATA_OAUTH_STATE = typeof ONADATA_OAUTH_STATE;
 
-export const GISIDA_MAPBOX_TOKEN = process.env.REACT_APP_GISIDA_MAPBOX_TOKEN || '';
-export type GISIDA_MAPBOX_TOKEN = typeof GISIDA_MAPBOX_TOKEN;
-
 export const GISIDA_ONADATA_API_TOKEN = process.env.REACT_APP_GISIDA_ONADATA_API_TOKEN || '';
 export type GISIDA_ONADATA_API_TOKEN = typeof GISIDA_ONADATA_API_TOKEN;
-
-export const DIGITAL_GLOBE_CONNECT_ID = process.env.REACT_APP_DIGITAL_GLOBE_CONNECT_ID || '';
-export type DIGITAL_GLOBE_CONNECT_ID = typeof DIGITAL_GLOBE_CONNECT_ID;
