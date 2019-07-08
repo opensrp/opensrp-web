56e17c146b94896a44ff145ab5bf2bb42b122507
p-netm <pmuriuki98@gmail.com>
2019-07-08 15:20:57 +0300

    Add actions and action creators

--- src/store/ducks/clients.ts
+++ src/store/ducks/clients.ts
@@ -9,6 +9,7 @@
  *      - getClientsArray
  *      - getClientsIdArray
  */
+import { keyBy } from 'lodash';
 import { AnyAction, Store } from 'redux';
 import SeamlessImmutable from 'seamless-immutable';
 
@@ -23,3 +24,27 @@ export interface Client {
   lastName: string;
   type: string;
 }
+
+// actions
+
+/** CLIENTS_FETCHED action type */
+export const CLIENTS_FETCHED = 'opensrp/reducer/clients/CLIENTS_FETCHED'; // ??What is authorize action in this context?
+
+/** interface for authorize action */
+export interface FetchClientsAction extends AnyAction {
+  clientsById: { [key: string]: Client };
+  type: typeof CLIENTS_FETCHED;
+}
+
+/** Create type for clients reducer actions */
+export type ClientsActionTypes = FetchClientsAction | AnyAction;
+
+// action Creators
+
+/** Fetch clients action creator
+ * @param {Client []} clientsList - clients array to add to store
+ */
+export const fetchClients = (clientsList: Client[] = []): FetchClientsAction => ({
+  clientsById: keyBy(clientsList, (client: Client) => client.id),
+  type: CLIENTS_FETCHED,
+});
