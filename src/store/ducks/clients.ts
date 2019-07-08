26516c9acfd2bc37c85530553a152d42ee837308
p-netm <pmuriuki98@gmail.com>
2019-07-08 14:55:51 +0300

    Add action-items for the clients reducer module

---
+++ src/store/ducks/clients.ts
@@ -0,0 +1,11 @@
+/** The clients reducer module:
+ * To implement:
+ *  - clients object interface
+ *  - action to fetch clients
+ *  - The client's reducer
+ *  - FetchClients action creator
+ *  - selectors:
+ *      - getclientById
+ *      - getClientsArray
+ *      - getClientsIdArray
+ */
