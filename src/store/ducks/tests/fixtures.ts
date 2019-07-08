2323a9f7152c2d23456a9a899c14f8c316e7a823
p-netm <pmuriuki98@gmail.com>
2019-07-08 19:20:01 +0300

    Add fixtures file for mock data during testing

---
+++ src/store/ducks/tests/fixtures.ts
@@ -0,0 +1,17 @@
+/** Mock data */
+import { Client } from '../clients';
+export const client1: Client = {
+  firstName: 'Khalesee',
+  gender: 'female',
+  homeAddress: 'Kings landing',
+  id: 'somelong uuid',
+  lastContactDate: 's8',
+  lastContactType: 'daggered',
+  lastName: 'Tagarryen',
+  location: 'the Red Keap',
+  middleName: 'mother of sth',
+  phonenumber: 'hahaha',
+  type: 'fierce',
+};
+
+export const clients = [client1];
