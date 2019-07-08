2813e8a9c100750f37a617e0798364b563d8b32a
p-netm <pmuriuki98@gmail.com>
2019-07-08 14:57:00 +0300

    Add initial thoughts on Clients Interface

--- src/store/ducks/clients.ts
+++ src/store/ducks/clients.ts
@@ -9,3 +9,17 @@
  *      - getClientsArray
  *      - getClientsIdArray
  */
+import { AnyAction, Store } from 'redux';
+import SeamlessImmutable from 'seamless-immutable';
+
+/** The reducer name */
+export const reducerName = 'clients';
+
+/** Interface for client object */
+export interface Client {
+  id: string;
+  firstName: string;
+  gender: string;
+  lastName: string;
+  type: string;
+}
