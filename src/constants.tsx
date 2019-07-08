9ac0f7f7dedc64a55be27b125eea62442294c0ab
p-netm <pmuriuki98@gmail.com>
2019-07-08 10:55:05 +0300

    Remove color unused color constants

--- src/constants.tsx
+++ src/constants.tsx
@@ -7,13 +7,3 @@ export const LOGOUT_URL = '/logout';
 export type LOGOUT_URL = typeof LOGOUT_URL;
 export const HOME_URL = '/';
 export type HOME_URL = typeof HOME_URL;
-
-// colors
-export const GREEN = 'Green';
-export type GREEN = typeof GREEN;
-export const YELLOW = 'Yellow';
-export type YELLOW = typeof YELLOW;
-export const ORANGE = 'Orange';
-export type ORANGE = typeof ORANGE;
-export const RED = 'Red';
-export type RED = typeof RED;
