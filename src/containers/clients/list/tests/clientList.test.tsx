067bded5be10664997795bff8b5ebe315042c14f
p-netm <pmuriuki98@gmail.com>
2019-07-08 19:19:24 +0300

    Update tests for Clientslist

--- src/containers/clients/list/tests/clientList.test.tsx
+++ src/containers/clients/list/tests/clientList.test.tsx
@@ -2,13 +2,14 @@ import reducerRegistry from '@onaio/redux-reducer-registry';
 import { create } from 'domain';
 import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
-import fetchMock = require('fetch-mock');
 import { createBrowserHistory } from 'history';
+import * as React from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router';
 import store from '../../../../store';
 import reducer, { fetchClients, reducerName } from '../../../../store/ducks/clients';
-import ConnectedClientsList, { ClientsList } from '../clientList';
+import * as fixtures from '../../../../store/ducks/tests/fixtures';
+import ConnectedClientsList, { ClientList } from '../clientList';
 
 reducerRegistry.register(reducerName, reducer);
 
@@ -22,18 +23,42 @@ describe('containers/clients/list/ClientList', () => {
   it('renders without crashing', () => {
     const mock: any = jest.fn();
     const props = {
-      history,
+      clientsArray: fixtures.clients,
+      fetchClientsActionCreator: mock,
       location: mock,
       match: mock,
     };
-    shallow(<ClientsList {...props} />);
+    shallow(<ClientList {...props} />);
   });
 
   it('renders without crashing', () => {
     const mock: any = jest.fn();
+    const props = {
+      clientsArray: fixtures.clients,
+      fetchClientsActionCreator: mock,
+      location: mock,
+      match: mock,
+    };
+    const wrapper = mount(<ClientList {...props} />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.unmount();
   });
 
   it('works correctly with the redux store', () => {
+    store.dispatch(fetchClients(fixtures.clients));
     const mock: any = jest.fn();
+    const props = {
+      fetchClientsActionCreator: mock,
+      location: mock,
+      match: mock,
+    };
+    const wrapper = mount(
+      <Provider store={store}>
+        <ConnectedClientsList {...props} />
+        );
+      </Provider>
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.unmount();
   });
 });
