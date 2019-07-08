57dfd2861fc609d461719b395efbca1de42555b8
p-netm <pmuriuki98@gmail.com>
2019-07-08 10:44:07 +0300

    Remove mocked envs specific to reveal;reiteration

--- src/configs/__mocks__/env.ts
+++ src/configs/__mocks__/env.ts
@@ -6,6 +6,3 @@
 
 export const SUPERSET_PLANS_SLICE = 0;
 export type SUPERSET_PLANS_SLICE = typeof SUPERSET_PLANS_SLICE;
-
-export const GISIDA_ONADATA_API_TOKEN = 'hunter2';
-export type GISIDA_ONADATA_API_TOKEN = typeof GISIDA_ONADATA_API_TOKEN;
