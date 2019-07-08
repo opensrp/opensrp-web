ceeee234b6640159a03261a067def73157d41441
p-netm <pmuriuki98@gmail.com>
2019-07-08 10:51:14 +0300

    Remove map icon

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,23 +1,8 @@
 // This is used to set up stuff for tests e.g. configs, icons, etc
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
-import { faMap } from '@fortawesome/free-solid-svg-icons';
 import enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 enzyme.configure({ adapter: new Adapter() });
-library.add(faMap);
 library.add(faUser);
-
-window.maps = [
-  {
-    _container: {
-      id: 'map-1',
-    },
-    isStyleLoaded: true,
-  } as any,
-];
-
-(window.URL as any) = {
-  createObjectURL: () => 'https://example.com',
-};
