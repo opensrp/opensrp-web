abd15aec4ef2d035b5a53be27db234db10ff9453
p-netm <pmuriuki98@gmail.com>
2019-07-09 19:17:20 +0300

    Updated fixture data with sample data form api

--- src/store/ducks/tests/fixtures.ts
+++ src/store/ducks/tests/fixtures.ts
@@ -1,17 +1,77 @@
 /** Mock data */
 import { Client } from '../clients';
+
 export const client1: Client = {
-  firstName: 'Khalesee',
-  gender: 'female',
-  homeAddress: 'Kings landing',
-  id: 'somelong uuid',
-  lastContactDate: 's8',
-  lastContactType: 'daggered',
-  lastName: 'Tagarryen',
-  location: 'the Red Keap',
-  middleName: 'mother of sth',
-  phonenumber: 'hahaha',
-  type: 'fierce',
+  firstName: 'Khumpai',
+  gender: '',
+  id: '9b67a82d-dac7-40c0-85aa-e5976339a6b6',
+  lastContactDate: '',
+  lastName: 'Family',
+  location: '',
+  middleName: '',
+  type: 'Client',
 };
 
-export const clients = [client1];
+export const client2: Client = {
+  firstName: 'Pramoj',
+  gender: '',
+  id: 'a30116d5-0612-419e-9b93-00c87df4ffbb',
+  lastContactDate: '',
+  lastName: 'Khumpai',
+  location: '',
+  middleName: '',
+  type: 'Client',
+};
+
+export const client3: Client = {
+  firstName: 'Sepsook',
+  gender: 'Male',
+  id: 'b23307c2-cbbb-481f-9cd7-f218564cc4bc',
+  lastContactDate: '',
+  lastName: 'Family',
+  location: '',
+  middleName: '',
+  type: 'Client',
+};
+export const client4: Client = {
+  firstName: 'Udom',
+  gender: '',
+  id: 'e5929901-9377-413c-850c-e41b48b34d5f',
+  lastContactDate: '',
+  lastName: 'Sepsook',
+  location: '',
+  middleName: '',
+  type: 'Client',
+};
+export const client5: Client = {
+  firstName: 'Imalda',
+  gender: 'Female',
+  id: 'faa436bb-0aa5-4c5f-a2fe-be687e164205',
+  lastContactDate: '',
+  lastName: 'DANE',
+  location: '',
+  middleName: '',
+  type: 'Client',
+};
+export const client6: Client = {
+  firstName: 'DANE',
+  gender: 'Male',
+  id: '0a5457f9-3e2e-4432-afae-75b57251651a',
+  lastContactDate: '',
+  lastName: 'Family',
+  location: '',
+  middleName: '',
+  type: 'Client',
+};
+export const client7 = {
+  firstName: 'Peter',
+  gender: '',
+  id: '6dab682a-0f31-4935-9114-c4d33d148617',
+  lastContactDate: '',
+  lastName: 'DANE',
+  location: '',
+  middleName: '',
+  type: 'Client',
+};
+
+export const clients: Client[] = [client1, client2, client3, client4, client5, client6, client7];
