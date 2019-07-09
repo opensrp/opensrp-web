11ebf9d044aead558ec2dfd939744e21e2c15d4f
p-netm <pmuriuki98@gmail.com>
2019-07-09 19:59:09 +0300

    Test client service response to ok status

--- src/services/clients/tests/index.test.ts
+++ src/services/clients/tests/index.test.ts
@@ -8,10 +8,20 @@ describe('services/clients', () => {
   jest.resetAllMocks();
 
   it('should return data for ok response status data', () => {
-    // returns data
+    const mockJsonPromise = Promise.resolve(fixtures.rawClients);
+    const mockFetchPromise = Promise.resolve({
+      json: () => mockJsonPromise,
+    });
+
+    jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
+
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    const responseData = ClientService.getClientsList();
+    expect(responseData).toEqual(fixtures.rawClients);
   });
 
   it('should handle different types of http errors', () => {
     // what does this method response look like for http errors
+    // How to mock status of a response after using fetch
   });
 });
