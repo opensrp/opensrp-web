eeb344a1bd410d5178214ac3ac184b39286c0cdf
p-netm <pmuriuki98@gmail.com>
2019-07-09 19:18:20 +0300

    Update test client selectors correctly fetch data

--- src/store/ducks/tests/clients.test.ts
+++ src/store/ducks/tests/clients.test.ts
@@ -1,4 +1,5 @@
 import reducerRegistry from '@onaio/redux-reducer-registry';
+import { keyBy, values } from 'lodash';
 import { FlushThunks } from 'redux-testkit';
 import store from '../../index';
 import reducer, {
@@ -9,6 +10,7 @@ import reducer, {
   getClientsIdArray,
   reducerName,
 } from '../clients';
+import * as fixtures from '../tests/fixtures';
 
 reducerRegistry.register(reducerName, reducer);
 
@@ -28,10 +30,42 @@ describe('reducers/clients', () => {
   });
 
   it('fetches clients correctly', () => {
-    store.dispatch(fetchClients([])); // put fixtures here
-  });
-
-  it('saves clients correctly', () => {
-    store.dispatch(fetchClients([])); // put fixture here
+    store.dispatch(fetchClients([fixtures.client1, fixtures.client2]));
+    expect(getClients(store.getState())).toEqual({
+      '9b67a82d-dac7-40c0-85aa-e5976339a6b6': {
+        firstName: 'Khumpai',
+        gender: '',
+        id: '9b67a82d-dac7-40c0-85aa-e5976339a6b6',
+        lastContactDate: '',
+        lastName: 'Family',
+        location: '',
+        middleName: '',
+        type: 'Client',
+      },
+      'a30116d5-0612-419e-9b93-00c87df4ffbb': {
+        firstName: 'Pramoj',
+        gender: '',
+        id: 'a30116d5-0612-419e-9b93-00c87df4ffbb',
+        lastContactDate: '',
+        lastName: 'Khumpai',
+        location: '',
+        middleName: '',
+        type: 'Client',
+      },
+    });
+    expect(getClientsArray(store.getState())).toEqual(values([fixtures.client1, fixtures.client2]));
+    expect(getClientsIdArray(store.getState())).toEqual(
+      [fixtures.client1, fixtures.client2].map(client => client.id)
+    );
+    expect(getClientById(store.getState(), '9b67a82d-dac7-40c0-85aa-e5976339a6b6')).toEqual({
+      firstName: 'Khumpai',
+      gender: '',
+      id: '9b67a82d-dac7-40c0-85aa-e5976339a6b6',
+      lastContactDate: '',
+      lastName: 'Family',
+      location: '',
+      middleName: '',
+      type: 'Client',
+    });
   });
 });
