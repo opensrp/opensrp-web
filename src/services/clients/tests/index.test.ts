11cc12db14844278085d4c990f2c33ed07e6eb43
p-netm <pmuriuki98@gmail.com>
2019-07-09 10:56:51 +0300

    Add proposal for initial clientservice tests

---
+++ src/services/clients/tests/index.test.ts
@@ -0,0 +1,17 @@
+import ClientService from '../';
+
+import * as fixtures from '../tests/fixtures';
+
+// How to mock the fetch method or is it the response
+
+describe('services/clients', () => {
+  jest.resetAllMocks();
+
+  it('should return data for ok response status data', () => {
+    // returns data
+  });
+
+  it('should handle different types of http errors', () => {
+    // what does this method response look like for http errors
+  });
+});
