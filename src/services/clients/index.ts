31708b8e7e797ecad22aafd991c39af7163d4415
p-netm <pmuriuki98@gmail.com>
2019-07-10 14:18:40 +0300

    Add error handling to getclientslist service

--- src/services/clients/index.ts
+++ src/services/clients/index.ts
@@ -4,20 +4,34 @@ class ClientService {
   private CLIENTS_API_ENDPOINT = `${OPENSRP_BASE_API_ENDPOINT}/${OPENSRP_CLIENT_ENDPOINT}/search`;
 
   public async getClientsList() {
-    const response = await fetch(this.CLIENTS_API_ENDPOINT, {
+    interface Response {
+      data: any;
+      error: string;
+    }
+    let result: Response | null = null;
+
+    await fetch(this.CLIENTS_API_ENDPOINT, {
       headers: {
         Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'cache-control': 'no-cache',
       },
       method: 'GET',
-    });
+    })
+      .then(async response => {
+        result = {
+          data: await response.json(),
+          error: (!!response.ok && `${response.status} Failed to retrieve clients list`) || '',
+        };
+      })
+      .catch(err => {
+        result = {
+          data: [],
+          error: err,
+        };
+      });
 
-    if (!response.ok) {
-      throw new Error(`${response.status} Failed to retrieve clients list`);
-    }
-
-    const data = await response.json();
-
-    return { data };
+    return result;
   }
 }
 
