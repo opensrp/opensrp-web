db20e94680469735757ae8e1171a3f9dae8924cc
p-netm <pmuriuki98@gmail.com>
2019-07-10 16:24:37 +0300

    mock fetch in clientsService tests

--- src/services/clients/tests/index.test.ts
+++ src/services/clients/tests/index.test.ts
@@ -2,26 +2,34 @@ import ClientService from '../';
 
 import * as fixtures from '../tests/fixtures';
 
-// How to mock the fetch method or is it the response
+// tslint:disable-next-line: no-var-requires
+const fetch = require('jest-fetch-mock');
 
 describe('services/clients', () => {
-  jest.resetAllMocks();
-
-  it('should return data for ok response status data', () => {
-    const mockJsonPromise = Promise.resolve(fixtures.rawClients);
-    const mockFetchPromise = Promise.resolve({
-      json: () => mockJsonPromise,
-    });
-
-    jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
-
-    expect(global.fetch).toHaveBeenCalledTimes(1);
-    const responseData = ClientService.getClientsList();
-    expect(responseData).toEqual(fixtures.rawClients);
+  beforeEach(() => {
+    jest.resetAllMocks();
+    fetch.resetMocks();
   });
 
-  it('should handle different types of http errors', () => {
-    // what does this method response look like for http errors
-    // How to mock status of a response after using fetch
+  it('should return data for ok response status data', async () => {
+    const data = [fixtures.rawClient1];
+    fetch.once(JSON.stringify(data));
+
+    const responseData = await ClientService.getClientsList();
+    expect(fetch.mock.calls.length).toEqual(1);
+    expect(responseData).toEqual({ data, error: '' });
+    expect(fetch.mock.calls[0][0]).toEqual(
+      'https://reveal-stage.smartregister.org/opensrp/rest/client/search'
+    );
+  });
+
+  it('should handle different types of http errors', async () => {
+    fetch.once('[]', { status: 500 });
+    const responseData = await ClientService.getClientsList();
+    const expected = {
+      data: [],
+      error: `${'500'} Failed to retrieve clients list`,
+    };
+    expect(responseData).toEqual(expected);
   });
 });
