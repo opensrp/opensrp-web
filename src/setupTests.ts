fd13b674f49e4a2f24f94bf82b9b5c9e40f1150e
p-netm <pmuriuki98@gmail.com>
2019-07-10 16:23:42 +0300

    Setup jest fetch mock

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,6 +3,12 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 import enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { GlobalWithFetchMock } from 'jest-fetch-mock';
 
 enzyme.configure({ adapter: new Adapter() });
 library.add(faUser);
+
+const customGlobal: GlobalWithFetchMock = global as GlobalWithFetchMock;
+/* tslint:disable-next-line no-var-requires */
+customGlobal.fetch = require('jest-fetch-mock');
+customGlobal.fetchMock = customGlobal.fetch;
