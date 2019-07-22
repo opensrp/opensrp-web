af2a57132c2939e96e90c1942ef80a47afc440fa
p-netm <pmuriuki98@gmail.com>
2019-07-22 10:57:26 +0300

    Encapsulate away response formatting of api calls response data

--- src/helpers/utils.tsx
+++ src/helpers/utils.tsx
@@ -1,6 +1,8 @@
 import { getOnadataUserInfo, getOpenSRPUserInfo } from '@onaio/gatekeeper';
 import { SessionState } from '@onaio/session-reducer';
 import { ONADATA_OAUTH_STATE, OPENSRP_OAUTH_STATE } from '../configs/env';
+import { FAILED_TO_RETRIEVE_CLIENTS } from '../constants';
+import { Client } from '../store/ducks/clients';
 
 /** Interface for an object that is allowed to have any property */
 export interface FlexObject {
@@ -21,5 +23,101 @@ export function oAuthUserInfoGetter(apiResponse: { [key: string]: any }): Sessio
       case ONADATA_OAUTH_STATE:
         return getOnadataUserInfo(apiResponse);
     }
+  }
+}
+
+/** interface for error property in case of error
+ * due to http error codes in service response
+ *
+ */
+interface HTTPError {
+  type: string;
+  apiErrorMessage: string;
+  serviceErrorMessage: string;
+  occurred: true;
+}
+
+/** interface for  error property in case of any other cause of error */
+interface OtherError {
+  type: string;
+  serviceErrorMessage: string;
+  occurred: true;
+}
+
+/** interface for error object if there was no error that occurred */
+interface NoError {
+  occurred: false;
+}
+
+/** interface for the service response */
+export interface ServiceResponse {
+  data: Client[];
+  error: HTTPError | OtherError | NoError;
+}
+
+/** DIRTY HACK -> need to investigate how to set the type of a response object from fetch */
+export function formatResponse(responseObj: any): ServiceResponse {
+  // Initialization assumes that the the fetch Call was successful
+  let serviceResponse: ServiceResponse = {
+    data: setData(responseObj),
+    error: {
+      occurred: false,
+    },
+  };
+
+  // define error structure for api erred response and for any other response error
+  if (responseObj.ok === false) {
+    // i think means http error
+    const error: HTTPError = {
+      apiErrorMessage: setApiErrorMessage(responseObj),
+      occurred: true,
+      serviceErrorMessage: setServiceErrorMessage(responseObj),
+      type: 'HTTP error',
+    };
+    serviceResponse = {
+      data: setData(responseObj),
+      error,
+    };
+  }
+
+  if (responseObj.ok === undefined) {
+    // means idk a network error; a system error, the fetch call did not get to the api
+    const error: OtherError = {
+      occurred: true,
+      serviceErrorMessage: setServiceErrorMessage(responseObj),
+      type: responseObj.name,
+    };
+    serviceResponse = {
+      data: setData(responseObj),
+      error,
+    };
+  }
+  return serviceResponse;
+}
+
+/**
+ * returns a string used as the apiErrorMessage in the serviceResponse object
+ * @param responseObj - response obj from fetch api call
+ */
+function setApiErrorMessage(responseObj: any): string {
+  return `HTTP Error: (${responseObj.status})`;
+}
+
+/**
+ * returns a string used as the serviceErrorMessage in the serviceResponse obj
+ */
+function setServiceErrorMessage(responseObj: any, message: string = ''): string {
+  return `${responseObj.status} ${FAILED_TO_RETRIEVE_CLIENTS}`;
+}
+
+/**
+ * Define the slice of data that is of interest from the fetch's call response data
+ */
+function setData(responseObj: any): Client[] {
+  const returnedData = responseObj.then((response: any) => response.json());
+  if (Array.isArray(returnedData)) {
+    return returnedData;
+  } else {
+    return [];
   }
 }
--- src/services/clients/index.ts
+++ src/services/clients/index.ts
@@ -1,64 +1,56 @@
 import { OPENSRP_BASE_API_ENDPOINT, OPENSRP_CLIENT_ENDPOINT } from '../../configs/env';
-import { FAILED_TO_RETRIEVE_CLIENTS, SERVICE_HEADERS } from '../../constants';
+import {
+  FAILED_TO_RETRIEVE_CLIENTS,
+  SERVICE_HEADERS,
+  UNKNOWN_API_ERROR_MESSAGE,
+  UNKNOWN_ERROR,
+} from '../../constants';
+import { formatResponse } from '../../helpers/utils';
 import { Client } from '../../store/ducks/clients';
 
-interface ApiErrorResponse {
-  error: string;
-  error_description: string;
+/** interface for error property in case of error
+ * due to http error codes in service response
+ */
+interface HTTPError {
+  type: string;
+  apiErrorMessage: string;
+  serviceErrorMessage: string;
+  occurred: true;
 }
 
+/** interface for  error property in case of any other cause of error */
+interface OtherError {
+  type: string;
+  serviceErrorMessage: string;
+  occurred: true;
+}
+
+/** interface for error object if there was no error that occurred */
+interface NoError {
+  occurred: false;
+}
+
+/** interface for the service response */
 interface ServiceResponse {
   data: Client[];
-  error: {
-    serviceErrorMessage: string;
-    error: string | null;
-    apiErrorMessage: string;
-  };
+  error: HTTPError | OtherError | NoError;
 }
 
 class ClientService {
   private CLIENTS_API_ENDPOINT = `${OPENSRP_BASE_API_ENDPOINT}/${OPENSRP_CLIENT_ENDPOINT}/search`;
 
-  public async getClientsList() {
-    let result: ServiceResponse | null = null;
-
-    await fetch(this.CLIENTS_API_ENDPOINT, { headers: { ...SERVICE_HEADERS }, method: 'GET' })
+  public getClientsList() {
+    let serviceResponse: ServiceResponse | null = null;
+    fetch(this.CLIENTS_API_ENDPOINT, { headers: { ...SERVICE_HEADERS }, method: 'GET' })
       .then(async response => {
-        const returnObj = await response.json();
-        result = this.formatResponse(returnObj);
+        serviceResponse = formatResponse(response);
       })
-      .catch(err => this.formatResponse(err));
+      .catch(err => {
+        serviceResponse = formatResponse(err);
+      });
 
-    return result;
-  }
-
-  /**
-   * @param {ApiErrorResponse | Client []} apiResponse - response from a resolved api promise call.
-   */
-  private formatResponse = (apiResponse: ApiErrorResponse | Client[]): ServiceResponse => {
-    // used to format the response object
-    let serviceResponse: ServiceResponse;
-    if (Array.isArray(apiResponse)) {
-      serviceResponse = {
-        data: apiResponse,
-        error: {
-          apiErrorMessage: '',
-          error: null,
-          serviceErrorMessage: '',
-        },
-      };
-    } else {
-      serviceResponse = {
-        data: [],
-        error: {
-          apiErrorMessage: apiResponse.error_description || 'Unknown Api Error Description',
-          error: apiResponse.error || 'Unknown Error',
-          serviceErrorMessage: `${(apiResponse as any).status} ${FAILED_TO_RETRIEVE_CLIENTS}`,
-        },
-      };
-    }
     return serviceResponse;
-  };
+  }
 }
 
 export default new ClientService();
