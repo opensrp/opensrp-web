3b903728fe6bf580f2e18f34bb475af7a8f516a7
p-netm <pmuriuki98@gmail.com>
2019-07-23 18:23:49 +0300

    Add envs for opensrp base urls and clients enpoint

--- src/configs/__mocks__/env.ts
+++ src/configs/__mocks__/env.ts
@@ -5,9 +5,9 @@
  */
 
 /** Base Api url for opensrp */
-export const OPENSRP_BASE_API_ENDPOINT = 'https://someip';
-export type OPENSRP_BASE_API_ENDPOINT = typeof OPENSRP_BASE_API_ENDPOINT;
+export const OPENSRP_API_BASE_URL = 'https://test.smartregister.org/opensrp/rest/';
+export type OPENSRP_API_BASE_URL = typeof OPENSRP_API_BASE_URL;
 
 /** the clients endpoint NOTE: does not end with / */
-export const OPENSRP_CLIENT_ENDPOINT = 'rest/client';
+export const OPENSRP_CLIENT_ENDPOINT = 'client';
 export type OPENSRP_CLIENTS_ENDPOINT = typeof OPENSRP_CLIENT_ENDPOINT;
