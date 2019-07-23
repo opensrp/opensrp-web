79d14636145daef5b860cdadf435d7c6e0a4c37e
p-netm <pmuriuki98@gmail.com>
2019-07-23 20:01:50 +0300

    Remove tests for dropped util function: extractclient

--- src/store/ducks/tests/clients.test.ts
+++ src/store/ducks/tests/clients.test.ts
@@ -1,7 +1,6 @@
 import reducerRegistry from '@onaio/redux-reducer-registry';
 import { values } from 'lodash';
 import { FlushThunks } from 'redux-testkit';
-import * as rawClientFixture from '../../../services/clients/tests/fixtures';
 import store from '../../index';
 import reducer, {
   fetchClients,
@@ -11,7 +10,6 @@ import reducer, {
   getClientsIdArray,
   reducerName,
 } from '../clients';
-import { extractClient } from '../clients';
 import * as fixtures from '../tests/fixtures';
 
 reducerRegistry.register(reducerName, reducer);
@@ -34,55 +32,15 @@ describe('reducers/clients', () => {
   it('fetches clients correctly', () => {
     store.dispatch(fetchClients([fixtures.client1, fixtures.client2]));
     expect(getClients(store.getState())).toEqual({
-      '9b67a82d-dac7-40c0-85aa-e5976339a6b6': {
-        firstName: 'Khumpai',
-        gender: '',
-        id: '9b67a82d-dac7-40c0-85aa-e5976339a6b6',
-        lastContactDate: '',
-        lastName: 'Family',
-        location: '',
-        middleName: '',
-        type: 'Client',
-      },
-      'a30116d5-0612-419e-9b93-00c87df4ffbb': {
-        firstName: 'Pramoj',
-        gender: '',
-        id: 'a30116d5-0612-419e-9b93-00c87df4ffbb',
-        lastContactDate: '',
-        lastName: 'Khumpai',
-        location: '',
-        middleName: '',
-        type: 'Client',
-      },
+      '9b67a82d-dac7-40c0-85aa-e5976339a6b6': fixtures.client1,
+      'a30116d5-0612-419e-9b93-00c87df4ffbb': fixtures.client2,
     });
     expect(getClientsArray(store.getState())).toEqual(values([fixtures.client1, fixtures.client2]));
     expect(getClientsIdArray(store.getState())).toEqual(
-      [fixtures.client1, fixtures.client2].map(client => client.id)
+      [fixtures.client1, fixtures.client2].map(client => client._id)
     );
-    expect(getClientById(store.getState(), '9b67a82d-dac7-40c0-85aa-e5976339a6b6')).toEqual({
-      firstName: 'Khumpai',
-      gender: '',
-      id: '9b67a82d-dac7-40c0-85aa-e5976339a6b6',
-      lastContactDate: '',
-      lastName: 'Family',
-      location: '',
-      middleName: '',
-      type: 'Client',
-    });
-  });
-
-  it('extracts clients correctly', () => {
-    const { rawClient1 } = rawClientFixture;
-    const expected = {
-      firstName: 'Khumpai',
-      gender: 'Male',
-      id: '9b67a82d-dac7-40c0-85aa-e5976339a6b6',
-      lastContactDate: '',
-      lastName: 'Family',
-      location: '',
-      middleName: '',
-      type: 'Client',
-    };
-    expect(extractClient(rawClient1)).toEqual(expected);
+    expect(getClientById(store.getState(), '9b67a82d-dac7-40c0-85aa-e5976339a6b6')).toEqual(
+      fixtures.client1
+    );
   });
 });
