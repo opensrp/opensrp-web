608d6ba4558caf08dd9597cc43ad7cc42f4d9427
p-netm <pmuriuki98@gmail.com>
2019-07-23 20:25:12 +0300

    Add a getClients function to clientsService

--- src/services/clients/index.ts
+++ src/services/clients/index.ts
@@ -1,7 +1,13 @@
 import { OPENSRP_CLIENT_ENDPOINT } from '../../configs/env';
+import { OpenSRPService } from '../../services/opensrp';
 
 class ClientService {
   private CLIENTS_API_ENDPOINT = `${OPENSRP_CLIENT_ENDPOINT}/search`;
+
+  public async getClientsList() {
+    const opensrpService = new OpenSRPService(this.CLIENTS_API_ENDPOINT);
+    return await opensrpService.list();
+  }
 }
 
 export default new ClientService();
