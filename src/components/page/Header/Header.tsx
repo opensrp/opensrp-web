a49a5b88a3d6c84bb6373533bf4b79ac3ccc6c76
HusnaHariz <husna@ona.io>
2019-08-22 18:03:58 +0300

    modify miecd header

--- src/components/page/Header/Header.tsx
+++ src/components/page/Header/Header.tsx
@@ -11,11 +11,10 @@ import {
   Nav,
   Navbar,
   NavbarToggler,
-  NavItem,
   UncontrolledDropdown,
 } from 'reactstrap';
 import logo from '../../../assets/images/logo.png';
-import { ENABLE_ABOUT, ENABLE_USERS, WEBSITE_NAME } from '../../../configs/env';
+import { ENABLE_ABOUT, ENABLE_CLIENTS, ENABLE_USERS, WEBSITE_NAME } from '../../../configs/env';
 import { CLIENT_URL, LOGIN_URL, LOGOUT_URL } from '../../../constants';
 import './Header.css';
 
@@ -56,64 +55,36 @@ export class HeaderComponent extends React.Component<HeaderProps, State> {
   public render() {
     const { authenticated, user } = this.props;
     return (
-      <div>
-        <Navbar color="light" light={true} expand="md">
-          <nav className="navbar navbar-expand-md navbar-light bg-light">
-            <Link to="/" className="navbar-brand">
-              <img src={logo} alt={WEBSITE_NAME} />
-            </Link>
-          </nav>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar={true}>
-            <Nav className="mr-auto" navbar={true}>
-              <NavItem>
-                <NavLink to="/" className="nav-link" activeClassName="active">
-                  Home
-                </NavLink>
-              </NavItem>
-              {ENABLE_USERS && (
-                <NavItem>
-                  <NavLink to="/404" className="nav-link" activeClassName="active">
-                    Users
-                  </NavLink>
-                </NavItem>
-              )}
-              {ENABLE_ABOUT && (
-                <NavItem>
-                  <NavLink to="/404" className="nav-link" activeClassName="active">
-                    About
-                  </NavLink>
-                </NavItem>
-              )}
-              <NavItem>
-                <NavLink to={CLIENT_URL} className="nav-link" activeClassName="active">
-                  Clients
-                </NavLink>
-              </NavItem>
-            </Nav>
-            <Nav className="ml-0" navbar={true}>
-              {authenticated ? (
-                <UncontrolledDropdown nav={true} inNavbar={true}>
-                  <DropdownToggle nav={true} caret={true}>
-                    <FontAwesomeIcon icon={['far', 'user']} /> {user.username}
-                  </DropdownToggle>
-                  <DropdownMenu right={true}>
-                    <DropdownItem>
-                      <NavLink to={LOGOUT_URL} className="nav-link" activeClassName="active">
-                        Sign Out
-                      </NavLink>
-                    </DropdownItem>
-                  </DropdownMenu>
-                </UncontrolledDropdown>
-              ) : (
-                <NavLink to={LOGIN_URL} className="nav-link" activeClassName="active">
-                  Login
-                </NavLink>
-              )}
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
+      <Navbar expand="md">
+        <nav id="image-settings">
+          <Link to="/">
+            <img src={logo} height="100%" width="100%" alt={WEBSITE_NAME} />
+          </Link>
+        </nav>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar={true}>
+          <Nav className="ml-auto" navbar={true}>
+            {authenticated ? (
+              <UncontrolledDropdown nav={true} inNavbar={true}>
+                <DropdownToggle nav={true} caret={true}>
+                  <FontAwesomeIcon icon={['far', 'user']} /> {user.username}
+                </DropdownToggle>
+                <DropdownMenu right={true}>
+                  <DropdownItem>
+                    <NavLink to={LOGOUT_URL} className="nav-link" activeClassName="active">
+                      Sign Out
+                    </NavLink>
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            ) : (
+              <NavLink to={LOGIN_URL} className="nav-link" activeClassName="active">
+                Login
+              </NavLink>
+            )}
+          </Nav>
+        </Collapse>
+      </Navbar>
     );
   }
 
