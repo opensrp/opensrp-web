eab94580fa5c1cfe84f47667bd12d36aa59f3526
HusnaHariz <husna@ona.io>
2019-08-22 18:05:01 +0300

    modify home page

--- src/containers/pages/Home/Home.tsx
+++ src/containers/pages/Home/Home.tsx
@@ -6,12 +6,65 @@ import './Home.css';
 class Home extends React.Component<{}, {}> {
   public render() {
     return (
-      <div className="text-center">
-        <Row className="welcome-box">
-          <Col>
-            <h3>Welcome to OpenSRp</h3>
-          </Col>
-        </Row>
+      <div className="home-main">
+        <div className="welcome-text">
+          <h1>Welcome to the pregnancy dashboard</h1>
+          <p>
+            This dashboard displays information collected from MIECD Viet Nam Pregnancy Module for
+            patients in your geographical location. The Module covers the whole pregnancy period
+            from conception to delivery and includes Pregnancy Registration, ANC visits, Birth
+            reports/Death reports, Risk Reports, Risk alerts and ResponseReports.
+          </p>
+        </div>
+        <div className="components-list">
+          <div className="spacer">
+            <div className="home-sub-containers">
+              <div className="sub-container-heading">
+                <p>Log face</p>
+              </div>
+              <div className="sub-container-message">
+                <div id="cont-size">
+                  <p>Display of all messages receioved from MIECD in chronological order</p>
+                </div>
+                <div id="cont-sized">
+                  <a href="/log-face">
+                    <button className="button-style">View</button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="spacer">
+            <div className="home-sub-containers">
+              <div className="sub-container-heading">
+                <p>Client</p>
+              </div>
+              <div className="sub-container-message">
+                <div id="cont-size">
+                  <p>This is the aggregation and categorization of patients data</p>
+                </div>
+                <div id="cont-sized">
+                  <button className="button-style">View</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="spacer">
+            <div className="home-sub-containers">
+              <div className="sub-container-heading">
+                <p>Analysis</p>
+              </div>
+              <div className="sub-container-message">
+                <div id="cont-size">
+                  <p>Important analysis and indicators generated from collected data</p>
+                </div>
+                <div id="cont-sized">
+                  <button className="button-style">View</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
