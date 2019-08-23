8f3a96598a3862b1d456de3d781ac17151a10857
HusnaHariz <husna@ona.io>
2019-08-23 10:25:50 +0300

    remove commented code

--- src/components/page/SideNav/sidenav.tsx
+++ src/components/page/SideNav/sidenav.tsx
@@ -19,48 +19,11 @@ import { ENABLE_ABOUT, ENABLE_CLIENTS, ENABLE_USERS, WEBSITE_NAME } from '../../
 import { CLIENT_URL, LOGIN_URL, LOGOUT_URL } from '../../../constants';
 import './sidenav.css';
 
-// /** interface for Header state */
-// interface State {
-//   isOpen: boolean;
-// }
-
-// /** interface for HeaderProps */
-// export interface HeaderProps extends RouteComponentProps {
-//   authenticated: boolean;
-//   user: User;
-// }
-
-// /** default props for Header */
-// const defaultHeaderProps: Partial<HeaderProps> = {
-//   authenticated: false,
-//   user: {
-//     email: '',
-//     name: '',
-//     username: '',
-//   },
-// };
-
-/** The Header component */
+/** The side navigation component */
 export class SidenavComponent extends React.Component {
-  //   public static defaultProps = defaultHeaderProps;
-
-  //   constructor(props: HeaderProps) {
-  //     super(props);
-
-  //     this.toggle = this.toggle.bind(this);
-  //     this.state = {
-  //       isOpen: false,
-  //     };
-  //   }
-
   public render() {
-    // const { authenticated, user } = this.props;
-    // const path = this.props.location.pathname;
     return (
       <div id="sidenav">
-        {/* <Navbar color="light" light={true} expand="md"> */}
-        {/* <NavbarToggler onClick={this.toggle} /> */}
-        {/* <Collapse isOpen={this.state.isOpen} navbar={true}> */}
         <Nav className="flex-column" navbar={true}>
           <NavItem>
             <NavLink
@@ -83,8 +46,6 @@ export class SidenavComponent extends React.Component {
             </NavLink>
           </NavItem>
         </Nav>
-        {/* </Collapse> */}
-        {/* </Navbar> */}
       </div>
     );
   }
