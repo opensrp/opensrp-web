2fa40f54de7f9a88d6fc3037c89646bb710710d0
Nafiz Ahmed <redoyahmed11@gmail.com>
2019-08-25 17:11:05 +0600

    Add new Households to existing Households in store

--- src/store/ducks/households.ts
+++ src/store/ducks/households.ts
@@ -80,7 +80,7 @@ export default function reducer(
     case HOUSEHOLDS_FETCHED:
       return SeamlessImmutable({
         ...state,
-        householdsById: action.householdsById,
+        householdsById: { ...state.householdsById, ...action.householdsById },
       });
     default:
       return state;
