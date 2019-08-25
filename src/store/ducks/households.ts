42a3e049cc321decf996fc943a5e8716eeeb0a4b
Nafiz Ahmed <redoyahmed11@gmail.com>
2019-08-25 15:55:39 +0600

    Add Household Interface

--- src/store/ducks/households.ts
+++ src/store/ducks/households.ts
@@ -5,3 +5,28 @@ import { FlexObject } from '../../helpers/utils';
 
 /** The reducer name */
 export const reducerName = 'households';
+
+/** Interface for household object as received from clientServices. Used Client instead of Household */
+export interface Household {
+  type: 'Client';
+  dateCreated: number;
+  serverVersion: number;
+  clientApplicationVersion: number;
+  clientDatabaseVersion: number;
+  baseEntityId: string;
+  identifiers: { [key: string]: string | null };
+  addresses: FlexObject[];
+  attributes: FlexObject;
+  firstName: string;
+  lastName: string;
+  birthdate: number;
+  middleName?: string;
+  birthdateApprox: boolean;
+  deathdateApprox: boolean;
+  gender?: string;
+  relationships: {
+    [key: string]: string[];
+  };
+  _id: string;
+  _rev: string;
+}
