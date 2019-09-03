775bef7d3f67b63673f0a5c0751825fc1980b965
Nafiz Ahmed <redoyahmed11@gmail.com>
2019-09-03 13:47:13 +0600

    Add top nav items to SideMenu

--- src/components/page/SideMenu/SideMenu.tsx
+++ src/components/page/SideMenu/SideMenu.tsx
@@ -1,4 +1,9 @@
 import * as React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Nav, Navbar, NavItem } from 'reactstrap';
+import { ENABLE_ABOUT, ENABLE_USERS } from '../../../configs/env';
+import { CLIENT_URL } from '../../../constants';
+import './SideMenu.css';
 
 class SideMenu extends React.Component<{}, {}> {
   constructor(props: {}) {
@@ -6,7 +11,38 @@ class SideMenu extends React.Component<{}, {}> {
   }
 
   public render() {
-    return <div>Side Menu</div>;
+    return (
+      <div>
+        <Navbar>
+          <Nav className="side-menu-container" navbar={true}>
+            <NavItem>
+              <NavLink to="/#" className="nav-link" activeClassName="active">
+                Home
+              </NavLink>
+            </NavItem>
+            {ENABLE_USERS && (
+              <NavItem>
+                <NavLink to="/404" className="nav-link" activeClassName="active">
+                  Users
+                </NavLink>
+              </NavItem>
+            )}
+            {ENABLE_ABOUT && (
+              <NavItem>
+                <NavLink to="/404" className="nav-link" activeClassName="active">
+                  About
+                </NavLink>
+              </NavItem>
+            )}
+            <NavItem>
+              <NavLink to={CLIENT_URL} className="nav-link" activeClassName="active">
+                Clients
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar>
+      </div>
+    );
   }
 }
 
