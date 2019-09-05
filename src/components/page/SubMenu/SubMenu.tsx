95d0629b762b098d28062dffb54477bbac38b008
Nafiz Ahmed <redoyahmed11@gmail.com>
2019-09-05 15:11:45 +0600

    Add static SubMenu component

--- src/components/page/SubMenu/SubMenu.tsx
+++ src/components/page/SubMenu/SubMenu.tsx
@@ -1,7 +1,58 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Collapse, Nav, NavbarToggler, NavItem } from 'reactstrap';
 import './SubMenu.css';
 
 export interface SubMenuProps {
   collapse: boolean;
 }
 
+export const defaultSubMenuProps: SubMenuProps = {
+  collapse: true,
+};
+
+export interface SubMenuState {
+  isCollapse: boolean;
+}
+
+class SubMenu extends React.Component<SubMenuProps, SubMenuState> {
+  public static defaultProps = defaultSubMenuProps;
+  constructor(props: SubMenuProps) {
+    super(props);
+    const { collapse } = this.props;
+    this.state = {
+      isCollapse: collapse,
+    };
+  }
+
+  public render() {
+    return (
+      <div>
+        <Nav>
+          <NavItem onClick={this.toggle}>
+            <a className="nav-link side-nav-item">
+              <FontAwesomeIcon icon={['far', 'user']} />
+              <span> Client Records </span>
+            </a>
+          </NavItem>
+        </Nav>
+        <Collapse isOpen={this.state.isCollapse}>
+          <Nav>
+            <NavItem>
+              <NavLink to="/" className="nav-link side-nav-item" activeClassName="side-nav-active">
+                <span> All Clients </span>
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </div>
+    );
+  }
+
+  private toggle = () => {
+    this.setState({ isCollapse: !this.state.isCollapse });
+  };
+}
+
+export default SubMenu;
