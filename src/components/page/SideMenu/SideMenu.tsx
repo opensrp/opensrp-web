30e336155025d825eb7cf1461d0f9c671a52b52c
Nafiz Ahmed <redoyahmed11@gmail.com>
2019-09-08 15:42:06 +0600

    Remove redundant side navbar items

--- src/components/page/SideMenu/SideMenu.tsx
+++ src/components/page/SideMenu/SideMenu.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Col, Nav, Navbar, NavItem, Row } from 'reactstrap';
-import { ENABLE_ABOUT, ENABLE_USERS } from '../../../configs/env';
 import {
   ADMIN_NAVIGATION_MODULE_OBJECT,
   CLIENT_NAVIGATION_MODULE_OBJECT,
@@ -65,46 +64,6 @@ class SideMenu extends React.Component<{}, SideMenuState> {
           <Col>
             <Navbar>
               <Nav navbar={true}>
-                <NavItem>
-                  <NavLink
-                    to="/"
-                    className="nav-link side-nav-item"
-                    activeClassName="side-nav-active"
-                  >
-                    Home
-                  </NavLink>
-                </NavItem>
-                {ENABLE_USERS && (
-                  <NavItem>
-                    <NavLink
-                      to="/404"
-                      className="nav-link side-nav-item"
-                      activeClassName="side-nav-active"
-                    >
-                      Users
-                    </NavLink>
-                  </NavItem>
-                )}
-                {ENABLE_ABOUT && (
-                  <NavItem>
-                    <NavLink
-                      to="/404"
-                      className="nav-link side-nav-item"
-                      activeClassName="side-nav-active"
-                    >
-                      About
-                    </NavLink>
-                  </NavItem>
-                )}
-                <NavItem>
-                  <NavLink
-                    to={CLIENT_URL}
-                    className="nav-link side-nav-item"
-                    activeClassName="side-nav-active"
-                  >
-                    Clients
-                  </NavLink>
-                </NavItem>
                 <SubMenu {...clientSubMenuProps} />
                 <SubMenu {...reportSubMenuProps} />
                 <SubMenu {...adminSubMenuProps} />
