c96e90b893455f6396749a8346cf65aa3a205ea3
Nafiz Ahmed <redoyahmed11@gmail.com>
2019-09-15 16:57:47 +0600

    Add SubMenu test to check method call

--- src/components/page/SubMenu/tests/SubMenu.test.tsx
+++ src/components/page/SubMenu/tests/SubMenu.test.tsx
@@ -60,4 +60,26 @@ describe('components/page/SubMenu', () => {
     expect(toJson(wrapper.find('SubMenu'))).toMatchSnapshot();
     wrapper.unmount();
   });
+
+  it('stimulates click and calls mock function properly', () => {
+    const mockCallBack = jest.fn();
+    const props: SubMenuProps = {
+      childNavs: [{ navLabel: 'Users', navURL: '/404' }],
+      identifier: 'ADMIN',
+      isCollapseMenuActive: true,
+      isExpand: false,
+      parentNav: { navIcon: ['fas', 'cog'], navLabel: 'Admin' },
+      setSideMenuToggle: mockCallBack,
+    };
+    const wrapper = mount(
+      <Router history={history}>
+        <SubMenu {...props} />
+      </Router>
+    );
+    wrapper.find('Nav .side-collapse-nav').simulate('click');
+    expect(mockCallBack.mock.calls.length).toEqual(1);
+    wrapper.find('Nav .side-collapse-nav').simulate('click');
+    expect(mockCallBack.mock.calls.length).toEqual(2);
+    wrapper.unmount();
+  });
 });
