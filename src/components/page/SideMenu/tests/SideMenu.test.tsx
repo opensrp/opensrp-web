bc5e57a420697da1a1d76ef717c81159fcb02894
Nafiz Ahmed <redoyahmed11@gmail.com>
2019-09-15 17:58:54 +0600

    Add interactive SideMenu tests

--- src/components/page/SideMenu/tests/SideMenu.test.tsx
+++ src/components/page/SideMenu/tests/SideMenu.test.tsx
@@ -3,6 +3,7 @@ import toJson from 'enzyme-to-json';
 import { createBrowserHistory } from 'history';
 import React from 'react';
 import { Router } from 'react-router';
+import { CLIENT_COLLAPSE_NAVIGATION_IDENTIFIER } from '../../../../constants';
 import SideMenu from '../SideMenu';
 
 const history = createBrowserHistory();
@@ -26,7 +27,47 @@ describe('components/page/SideMenu', () => {
         <SideMenu />
       </Router>
     );
-    expect(toJson(wrapper.find('SideMenu'))).toMatchSnapshot();
+    /** client SubMenu renders correctly */
+    expect(toJson(wrapper.find('SubMenu').first())).toMatchSnapshot();
+    wrapper.unmount();
+  });
+
+  it('renders expanded menus correctly', () => {
+    const wrapper = mount(
+      <Router history={history}>
+        <SideMenu />
+      </Router>
+    );
+    /** expands first SubMenu */
+    wrapper
+      .find('Nav .side-collapse-nav')
+      .first()
+      .simulate('click');
+    expect(toJson(wrapper.find('Collapse').first())).toMatchSnapshot();
+    /** collapse first SubMenu */
+    wrapper
+      .find('Nav .side-collapse-nav')
+      .first()
+      .simulate('click');
+    expect(toJson(wrapper.find('Collapse').first())).toMatchSnapshot();
+    wrapper.unmount();
+  });
+
+  it('manages state correctly', () => {
+    const wrapper = mount(
+      <Router history={history}>
+        <SideMenu />
+      </Router>
+    );
+    wrapper.find('SideMenu').simulate('click');
+    expect(wrapper.find('SideMenu').state('expandedNavs')).toEqual([]);
+    wrapper
+      .find('Nav .side-collapse-nav')
+      .first()
+      .simulate('click');
+    expect(wrapper.find('SideMenu').state('expandedNavs')).toEqual([
+      CLIENT_COLLAPSE_NAVIGATION_IDENTIFIER,
+    ]);
     wrapper.unmount();
   });
 });
