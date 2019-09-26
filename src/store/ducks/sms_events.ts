7111ac1436625cd18bf21ff94bd29fb3b2746bf9
FrankApiyo <franklineapiyo@gmail.com>
2019-09-26 12:50:16 +0300

    Change smsReducer to PascalCase

--- src/store/ducks/sms_events.ts
+++ src/store/ducks/sms_events.ts
@@ -2,11 +2,11 @@ import { get, keyBy, keys, values } from 'lodash';
 import { AnyAction, Store } from 'redux';
 
 /** The reducer name */
-export const reducerName = 'smsReducer';
+export const reducerName = 'SmsReducer';
 
 /** Interface for SMS record object as received from discover */
 // tslint:disable-next-line: class-name
-export interface smsReducer {
+export interface SmsReducer {
   age: string;
   EventDate: string;
   event_id: string;
@@ -26,7 +26,7 @@ export const FETCHED_SMS = 'opensrp/reducer/FETCHED_SMS';
 /** interface for sms fetch */
 // tslint:disable-next-line: class-name
 export interface fetchSmsInterface extends AnyAction {
-  payload: { [key: string]: smsReducer };
+  payload: { [key: string]: SmsReducer };
   type: typeof FETCHED_SMS;
 }
 
@@ -39,9 +39,9 @@ export type TestActionTypes = fetchSmsInterface | AnyAction;
  * @param {Client []} clientsList - clients array to add to store
  * @return {FetchClientsAction} - an action to add clients to redux store
  */
-export const fetchSms = (TrialData: smsReducer[] = []): fetchSmsInterface => {
+export const fetchSms = (TrialData: SmsReducer[] = []): fetchSmsInterface => {
   const actionCreated = {
-    payload: keyBy(TrialData, (trialObj: smsReducer) => trialObj.event_id),
+    payload: keyBy(TrialData, (trialObj: SmsReducer) => trialObj.event_id),
     type: FETCHED_SMS as typeof FETCHED_SMS,
   };
   //   console.log("gets called", actionCreated);
@@ -53,7 +53,7 @@ export const fetchSms = (TrialData: smsReducer[] = []): fetchSmsInterface => {
 /** interface for sms state in redux store */
 // tslint:disable-next-line: class-name
 interface smsStateReducer {
-  payload: { [key: string]: smsReducer };
+  payload: { [key: string]: SmsReducer };
 }
 
 /** initial sms-state state */
@@ -83,6 +83,6 @@ export default function reducer(
  * @param {Partial<Store>} state - the redux store
  * @return { { [key: string] : Client} } - clients object as values, reepective ids as keys
  */
-export function getTestData(state: Partial<Store>): smsReducer[] {
+export function getTestData(state: Partial<Store>): SmsReducer[] {
   return values((state as any)[reducerName].payload);
 }
