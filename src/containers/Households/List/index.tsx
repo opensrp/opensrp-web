371c223654505ed2ddfcf1dc49c235d6b561684a
Nafiz Ahmed <redoyahmed11@gmail.com>
2019-09-26 16:42:06 +0600

    Add HouseholdList component

--- src/containers/Households/List/index.tsx
+++ src/containers/Households/List/index.tsx
@@ -1,7 +1,15 @@
 import reducerRegistry from '@onaio/redux-reducer-registry';
+import { some } from 'lodash';
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { Table } from 'reactstrap';
+import { Store } from 'redux';
+import Loading from '../../../components/page/Loading';
+import { OPENSRP_HOUSEHOLD_ENDPOINT } from '../../../configs/env';
 import { OpenSRPService } from '../../../services/opensrp';
 import clientsReducer, {
   fetchHouseholds,
+  getHouseholdsArray,
   Household,
   reducerName as clientsReducerName,
 } from '../../../store/ducks/clients';
@@ -23,3 +31,89 @@ export const defaultHouseholdListProps: HouseholdListProps = {
   householdsArray: [],
   opensrpService: OpenSRPService,
 };
+
+/** Display the Household list  */
+class HouseholdList extends React.Component<HouseholdListProps, {}> {
+  public static defaultProps: HouseholdListProps = defaultHouseholdListProps;
+  constructor(props: HouseholdListProps) {
+    super(props);
+  }
+
+  public async componentDidMount() {
+    const { fetchHouseholdsActionCreator, opensrpService } = this.props;
+    const params = { clientType: 'ec_household', pageNumber: '0', pageSize: '50' };
+    const clientService = new opensrpService(`${OPENSRP_HOUSEHOLD_ENDPOINT}`);
+    const response = await clientService.list(params);
+    fetchHouseholdsActionCreator(response.clients);
+  }
+
+  public render() {
+    /** render loader if there are no households in state */
+    const { householdsArray } = this.props;
+    if (!some(householdsArray)) {
+      return <Loading />;
+    }
+    return (
+      <div>
+        <Table striped={true}>
+          <thead>
+            <tr>
+              <th>HH ID Number</th>
+              <th>Family Name</th>
+              <th>Head of Household</th>
+              <th>Phone</th>
+              <th>Registered Date</th>
+              <th>Members</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {householdsArray.map((household: Household) => {
+              return (
+                <tr key={household.baseEntityId}>
+                  <td>{household.baseEntityId}</td>
+                  <td>{household.lastName}</td>
+                  <td>{household.attributes.HouseholdHead}</td>
+                  <td>{household.attributes.phoneNumber}</td>
+                  <td>{household.dateCreated}</td>
+                  <td>{household.attributes.memberCount}</td>
+                  <td>
+                    <a href={`${'#'}`}> view </a>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
+}
+
+export { HouseholdList };
+/** Maybe define default props */
+/** connect the component to the store */
+
+/** Interface to describe props from mapStateToProps */
+interface DispatchedStateProps {
+  householdsArray: Household[];
+}
+
+/** Map props to state  */
+const mapStateToProps = (state: Partial<Store>): DispatchedStateProps => {
+  const result = {
+    householdsArray: getHouseholdsArray(state),
+  };
+  return result;
+};
+
+/** map props to actions */
+const mapDispatchToProps = { fetchHouseholdsActionCreator: fetchHouseholds };
+
+/** connect householdsList to the redux store */
+const ConnectedHouseholdList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(HouseholdList);
+
+export default ConnectedHouseholdList;
