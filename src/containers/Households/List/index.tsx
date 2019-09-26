a939461aa7503f294744903796ec7fbd479546bf
Nafiz Ahmed <redoyahmed11@gmail.com>
2019-09-26 12:47:31 +0600

    Add HouseholdListProps Interface

---
+++ src/containers/Households/List/index.tsx
@@ -0,0 +1,18 @@
+import reducerRegistry from '@onaio/redux-reducer-registry';
+import { OpenSRPService } from '../../../services/opensrp';
+import clientsReducer, {
+  fetchHouseholds,
+  Household,
+  reducerName as clientsReducerName,
+} from '../../../store/ducks/clients';
+import './householdList.css';
+
+/** register the clients reducer */
+reducerRegistry.register(clientsReducerName, clientsReducer);
+
+/** props Interface for the householdList component */
+export interface HouseholdListProps {
+  opensrpService: typeof OpenSRPService;
+  householdsArray: Household[];
+  fetchHouseholdsActionCreator: typeof fetchHouseholds;
+}
