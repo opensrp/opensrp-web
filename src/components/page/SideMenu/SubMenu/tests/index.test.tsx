d1a18fc66f8b643e6c16a2405147c0ddf17e7cdc
p-netm <pmuriuki98@gmail.com>
2019-10-02 13:07:54 +0300

    Refactor tests for submenu component

--- src/components/page/SideMenu/SubMenu/tests/index.test.tsx
+++ src/components/page/SideMenu/SubMenu/tests/index.test.tsx
@@ -1,8 +1,11 @@
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { mount, shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import { createBrowserHistory } from 'history';
 import React from 'react';
 import { Router } from 'react-router';
-import { SubMenu, SubMenuProps } from '..';
+import snapshotDiff from 'snapshot-diff';
+import SubMenu, { SubMenuProps } from '..';
 
 const history = createBrowserHistory();
 
@@ -12,69 +15,93 @@ describe('components/page/SubMenu', () => {
   });
 
   it('renders without crashing', () => {
-    const props: SubMenuProps = {
-      history,
+    const mock: any = jest.fn();
+    const props = {
       childNavs: [],
       collapsedModuleLabel: 'All client Records',
-      parentNav: { icon: ['far', 'user'], label: 'All client Records' },
+      history,
+      location: mock,
+      match: mock,
+      parentNav: { icon: ['far', 'user'] as IconProp, label: 'All client Records' },
     };
-    shallow(<SubMenu {...props} />);
+    shallow(
+      <Router history={history}>
+        <SubMenu {...props} />
+      </Router>
+    );
   });
 
   it('renders subMenu correctly when in collapsed state', () => {
-    const props: SubMenuProps = {
+    const mock: any = jest.fn();
+    const props = {
       childNavs: [{ label: 'Users', url: '/users' }, { label: 'Roles', url: '/roles' }],
       collapsedModuleLabel: 'Admin',
-      parentNav: { icon: ['fas', 'cog'], label: 'Admin' },
+      history,
+      location: { pathname: '/users' },
+      match: mock,
+      parentNav: { icon: ['fas', 'cog'] as IconProp, label: 'Admin' },
     };
-    const wrapper = mount(<SubMenu {...props} />);
+
+    const wrapper = mount(
+      <Router history={history}>
+        <SubMenu {...props} />
+      </Router>
+    );
     // parent nav
     const parentNav = wrapper.find('.collapse-menu-title');
     expect(parentNav.length).toEqual(1);
 
     // the child navs
-    const usersNav = wrapper.find('a[href="/users"]');
+    const usersNav = wrapper.find('div.collapse.show a[href="/users"]');
     expect(usersNav.length).toEqual(1);
 
-    const rolesNav = wrapper.find('a[href="/roles"]');
+    const rolesNav = wrapper.find('div.collapse.show a[href="/roles"]');
     expect(rolesNav.length).toEqual(1);
+
     wrapper.unmount();
   });
 
-  it('toggles between collapsed and closed state', () => {
-    const props: SubMenuProps = {
+  it('renders subMenu correctly when in closed state', () => {
+    const mock: any = jest.fn();
+    const props = {
       childNavs: [{ label: 'Users', url: '/users' }, { label: 'Roles', url: '/roles' }],
       collapsedModuleLabel: '',
-      parentNav: { icon: ['fas', 'cog'], label: 'Admin' },
+      history,
+      location: { pathname: '/users' },
+      match: mock,
+      parentNav: { icon: ['fas', 'cog'] as IconProp, label: 'Admin' },
     };
 
-    const wrapper = mount(<Router history={history} >
-    <SubMenu {...props} />)
-    </Router>;
+    const wrapper = mount(
+      <Router history={history}>
+        <SubMenu {...props} />
+      </Router>
+    );
+    // parent nav
+    const parentNav = wrapper.find('.collapse-menu-title');
+    expect(parentNav.length).toEqual(1);
 
-    // starts with submenu as closed, not collapsed
-    let usersNav = wrapper.find('a[href="/users"]');
+    // the child navs
+    const usersNav = wrapper.find('div.collapse.show a[href="/users"]');
     expect(usersNav.length).toEqual(0);
 
-    // clicking on parent nav negates the above
-    const parentNav = wrapper.find('.side-collapse-nav');
-    expect(parentNav.length).toBeGreaterThanOrEqual(1);
-    parentNav.simulate('click');
-    wrapper.update();
-
-    // after the collapse childNavs are now visible
-    usersNav = wrapper.find('a[href="/users"]');
-    expect(usersNav.length).toEqual(1);
+    const rolesNav = wrapper.find('div.collapse.show a[href="/roles"]');
+    expect(rolesNav.length).toEqual(0);
 
     wrapper.unmount();
   });
 
   it('stimulates click and calls mock function properly', () => {
     const mockCallBack = jest.fn();
-    const props: SubMenuProps = {
-      childNavs: [{ label: 'Users', url: '/users' }],
-      collapsedModuleLabel: 'Admin',
-      parentNav: { icon: ['fas', 'cog'], label: 'Admin' },
+    const mock = jest.fn();
+    const pathname = '/somePath';
+    const props = {
+      childNavs: [{ label: 'Users', url: '/users' }, { label: 'Roles', url: '/roles' }],
+      collapsedModuleLabel: '',
+      history,
+      location: { pathname },
+      match: mock,
+      parentNav: { icon: ['fas', 'cog'] as IconProp, label: 'Admin' },
       setCollapsedModuleLabel: mockCallBack,
     };
     const wrapper = mount(
