b9898176a8c293ed28529f5fe8c499d0e18e4fcb
FrankApiyo <franklineapiyo@gmail.com>
2019-10-07 15:37:37 +0300

    Add .css to patientDetails

--- src/containers/PatientDetails/index.tsx
+++ src/containers/PatientDetails/index.tsx
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
+import { Container, Row } from 'reactstrap';
+import BasicInformation from '../../components/BasicInformation';
 import MotherWeightChart from '../../components/MotherWeightChart';
+import './index.css';
 
+const basicInformationProps = {
+  currentEdd: '30.06.2019',
+  currentGravidity: 2,
+  currentParity: 1,
+  id: '134235425',
+  location: 'Kon Tum',
+  previousPregnancyRisk: 'Convulsion, Caeserian',
+};
 class PatientDetails extends Component {
   public render() {
     return (
-      <div className={'container-fluid'}>
-        <div className={'row'}>basic information</div>
-        <div className={'row'}>reports</div>
-        <div className={'row'}>
+      <Container fluid={true} id={'patient-details'}>
+        <Row>
+          <BasicInformation {...basicInformationProps} />
+        </Row>
+        <Row>reports</Row>
+        <Row id={'chart'}>
           <MotherWeightChart />
-        </div>
-      </div>
+        </Row>
+      </Container>
     );
   }
 }
