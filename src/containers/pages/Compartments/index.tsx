f716141a33b053da735c689cd530291caa0eb1cc
FrankApiyo <franklineapiyo@gmail.com>
2019-10-14 14:09:38 +0300

    Add a Compartments component

---
+++ src/containers/pages/Compartments/index.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Compartments extends Component<{}, {}> {
+  public render() {
+    return <div>this is the compartment</div>;
+  }
+}
+
+export default Compartments;
