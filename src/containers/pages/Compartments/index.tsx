afc8a4d81c3ad917027d61ab76a750072286b3a4
FrankApiyo <franklineapiyo@gmail.com>
2019-10-15 17:04:00 +0300

    Pass route arguments on to HierarchichalDataTable

--- src/containers/pages/Compartments/index.tsx
+++ src/containers/pages/Compartments/index.tsx
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import DataCircleCard from '../../../components/DataCircleCard';
 import HierarchichalDataTable from '../../../components/HierarchichalDataTable';
 
-class Compartments extends Component<{}, {}> {
+class Compartments extends Component<any, {}> {
   public render() {
-    return <HierarchichalDataTable />;
+    return (
+      <HierarchichalDataTable
+        current_level={parseInt(this.props.match.params.current_level, 10)}
+        node_id={this.props.match.params.node_id}
+        direction={this.props.match.params.direction}
+      />
+    );
   }
 }
 
