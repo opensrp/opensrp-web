c3f7d3a2658f1e52adeebf982c3fdea6bc70abac
p-netm <pmuriuki98@gmail.com>
2019-10-18 12:55:13 +0300

    Export only required variables from navigationconfig

--- src/configs/navigationConfigs.ts
+++ src/configs/navigationConfigs.ts
@@ -1,11 +1,6 @@
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { SideMenuProps } from '../components/page/SideMenu';
-import {
-  ModulePageLink,
-  NavigationModule,
-  PageLink,
-  SubMenuProps,
-} from '../components/page/SideMenu/SubMenu';
+import { ModulePageLink, NavigationModule, PageLink } from '../components/page/SideMenu/SubMenu';
 import {
   ADMIN,
   ALL_CLIENTS,
@@ -42,63 +37,63 @@ import {
 
 // Page links
 
-export const CLIENT_PAGE_NAVIGATION: PageLink = {
+const CLIENT_PAGE_NAVIGATION: PageLink = {
   label: ALL_CLIENTS,
   url: CLIENT_URL,
 };
-export const HOUSEHOLD_PAGE_NAVIGATION: PageLink = {
+const HOUSEHOLD_PAGE_NAVIGATION: PageLink = {
   label: HOUSEHOLD,
   url: HOUSEHOLD_URL,
 };
-export const ANC_PAGE_NAVIGATION: PageLink = {
+const ANC_PAGE_NAVIGATION: PageLink = {
   label: ANC,
   url: ANC_URL,
 };
-export const CHILD_PAGE_NAVIGATION: PageLink = {
+const CHILD_PAGE_NAVIGATION: PageLink = {
   label: CHILD,
   url: CHILD_URL,
 };
 
-export const USERS_PAGE_NAVIGATION: PageLink = {
+const USERS_PAGE_NAVIGATION: PageLink = {
   label: USERS,
   url: USER_URL,
 };
-export const ROLES_PAGE_NAVIGATION: PageLink = {
+const ROLES_PAGE_NAVIGATION: PageLink = {
   label: ROLES,
   url: ROLE_URL,
 };
-export const TEAMS_PAGE_NAVIGATION: PageLink = {
+const TEAMS_PAGE_NAVIGATION: PageLink = {
   label: TEAMS,
   url: TEAM_URL,
 };
-export const LOCATIONS_PAGE_NAVIGATION: PageLink = {
+const LOCATIONS_PAGE_NAVIGATION: PageLink = {
   label: LOCATIONS,
   url: LOCATIONS_URL,
 };
 
 // icons
-export const clientModuleNavIcon: IconProp = ['far', 'user'];
-export const reportModuleNavIcon: IconProp = ['fas', 'chart-line'];
-export const adminModuleNavIcon: IconProp = ['fas', 'cog'];
+const clientModuleNavIcon: IconProp = ['far', 'user'];
+const reportModuleNavIcon: IconProp = ['fas', 'chart-line'];
+const adminModuleNavIcon: IconProp = ['fas', 'cog'];
 
 // Navigation links for navigation module. collapses when clicked to display the pageLinks
 
-export const CLIENT_MODULE_PARENT_NAV: ModulePageLink = {
+const CLIENT_MODULE_PARENT_NAV: ModulePageLink = {
   icon: clientModuleNavIcon,
   label: CLIENT_RECORDS,
 };
-export const REPORT_MODULE_PARENT_NAV: ModulePageLink = {
+const REPORT_MODULE_PARENT_NAV: ModulePageLink = {
   icon: reportModuleNavIcon,
   label: REPORTS,
   url: REPORTS_URL,
 };
-export const ADMIN_MODULE_PARENT_NAV: ModulePageLink = {
+const ADMIN_MODULE_PARENT_NAV: ModulePageLink = {
   icon: adminModuleNavIcon,
   label: ADMIN,
 };
 
 // nav module objects
-export const CLIENT_NAVIGATION_MODULE: NavigationModule = {
+const CLIENT_NAVIGATION_MODULE: NavigationModule = {
   childNavs: [
     ENABLE_CLIENT_PAGE && CLIENT_PAGE_NAVIGATION,
     ENABLE_HOUSEHOLD_PAGE && HOUSEHOLD_PAGE_NAVIGATION,
@@ -107,11 +102,11 @@ export const CLIENT_NAVIGATION_MODULE: NavigationModule = {
   ].filter((childNav): childNav is PageLink => typeof childNav !== 'boolean'),
   parentNav: CLIENT_MODULE_PARENT_NAV,
 };
-export const REPORT_NAVIGATION_MODULE: NavigationModule = {
+const REPORT_NAVIGATION_MODULE: NavigationModule = {
   childNavs: [],
   parentNav: REPORT_MODULE_PARENT_NAV,
 };
-export const ADMIN_NAVIGATION_MODULE: NavigationModule = {
+const ADMIN_NAVIGATION_MODULE: NavigationModule = {
   childNavs: [
     ENABLE_USER_PAGE && USERS_PAGE_NAVIGATION,
     ENABLE_ROLE_PAGE && ROLES_PAGE_NAVIGATION,
@@ -124,14 +119,14 @@ export const ADMIN_NAVIGATION_MODULE: NavigationModule = {
 };
 
 /** Enable Clients Module from its child pages */
-export const ENABLE_CLIENT_RECORDS_MODULE =
+const ENABLE_CLIENT_RECORDS_MODULE =
   ENABLE_ANC_PAGE || ENABLE_CLIENT_PAGE || ENABLE_HOUSEHOLD_PAGE || ENABLE_HOUSEHOLD_PAGE;
-export type ENABLE_CLIENT_RECORDS_MODULE = typeof ENABLE_CLIENT_RECORDS_MODULE;
+type ENABLE_CLIENT_RECORDS_MODULE = typeof ENABLE_CLIENT_RECORDS_MODULE;
 
 /** Enable Admin Module from its child pages */
-export const ENABLE_ADMIN_MODULE =
+const ENABLE_ADMIN_MODULE =
   ENABLE_USER_PAGE || ENABLE_ROLE_PAGE || ENABLE_TEAM_PAGE || ENABLE_LOCATION_PAGE;
-export type ENABLE_ADMIN_MODULE = typeof ENABLE_ADMIN_MODULE;
+type ENABLE_ADMIN_MODULE = typeof ENABLE_ADMIN_MODULE;
 
 export const sideMenuProps: SideMenuProps = {
   navigationModules: [
