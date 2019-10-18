16c8f471edf36192574e4795402878295184ccc3
FrankApiyo <franklineapiyo@gmail.com>
2019-10-18 11:45:19 +0300

    Refactor markup for DataCircleCard

--- src/components/DataCircleCard/index.tsx
+++ src/components/DataCircleCard/index.tsx
@@ -15,30 +15,28 @@ function DataCircleCard({ highRisk, lowRisk, noRisk, title }: Props) {
   return (
     <Col lg="3">
       <Card className="dataCircleCard">
+        <CardTitle>{title}</CardTitle>
         <CardBody>
-          <CardTitle>{title}</CardTitle>
-          <CardText>
-            <ul className="circlesRow">
-              <li className="red">
-                <Link to={`${HIERARCHICAL_DATA_URL}/high-risk/${title}`}>
-                  <span className="number">{highRisk}</span>
-                </Link>
-                <span className="risk-level">High Risk</span>
-              </li>
-              <li className="orange">
-                <Link to={`${HIERARCHICAL_DATA_URL}/low-risk/${title}`}>
-                  <span className="number">{lowRisk}</span>
-                </Link>
-                <span className="risk-level">Low Risk</span>
-              </li>
-              <li className="green">
-                <Link to={`${HIERARCHICAL_DATA_URL}/no-risk/${title}`}>
-                  <span className="number">{noRisk}</span>
-                </Link>
-                <span className="risk-level">No Risk</span>
-              </li>
-            </ul>
-          </CardText>
+          <ul className="circlesRow">
+            <li className="red">
+              <Link to={`${HIERARCHICAL_DATA_URL}/high-risk/${title}`}>
+                <span className="number">{highRisk}</span>
+              </Link>
+              <span className="risk-level">High Risk</span>
+            </li>
+            <li className="orange">
+              <Link to={`${HIERARCHICAL_DATA_URL}/low-risk/${title}`}>
+                <span className="number">{lowRisk}</span>
+              </Link>
+              <span className="risk-level">Low Risk</span>
+            </li>
+            <li className="green">
+              <Link to={`${HIERARCHICAL_DATA_URL}/no-risk/${title}`}>
+                <span className="number">{noRisk}</span>
+              </Link>
+              <span className="risk-level">No Risk</span>
+            </li>
+          </ul>
         </CardBody>
       </Card>
     </Col>
