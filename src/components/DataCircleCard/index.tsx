ebd35930333d6cf754c90bc5d0c9f5142ef51193
FrankApiyo <franklineapiyo@gmail.com>
2019-10-18 11:24:35 +0300

    Pass title in DataCircleCard

--- src/components/DataCircleCard/index.tsx
+++ src/components/DataCircleCard/index.tsx
@@ -20,19 +20,19 @@ function DataCircleCard({ highRisk, lowRisk, noRisk, title }: Props) {
           <CardText>
             <ul className="circlesRow">
               <li className="red">
-                <Link to={`${HIERARCHICAL_DATA_URL}/high-risk`}>
+                <Link to={`${HIERARCHICAL_DATA_URL}/high-risk/${title}`}>
                   <span className="number">{highRisk}</span>
                 </Link>
                 <span className="risk-level">High Risk</span>
               </li>
               <li className="orange">
-                <Link to={`${HIERARCHICAL_DATA_URL}/low-risk`}>
+                <Link to={`${HIERARCHICAL_DATA_URL}/low-risk/${title}`}>
                   <span className="number">{lowRisk}</span>
                 </Link>
                 <span className="risk-level">Low Risk</span>
               </li>
               <li className="green">
-                <Link to={`${HIERARCHICAL_DATA_URL}/no-risk`}>
+                <Link to={`${HIERARCHICAL_DATA_URL}/no-risk/${title}`}>
                   <span className="number">{noRisk}</span>
                 </Link>
                 <span className="risk-level">No Risk</span>
