7ac6aaa4a5ae8e327bdac541ba6358e0a53a0467
FrankApiyo <franklineapiyo@gmail.com>
2019-10-22 15:08:40 +0300

    Use constants in BasicInformation, HierarchichalDataTable and PatientDetails

--- src/components/BasicInformation/index.tsx
+++ src/components/BasicInformation/index.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import { Card, CardTitle, Col, Row } from 'reactstrap';
+import {
+  BASIC_INFORMATION,
+  CURRENT_EDD,
+  CURRENT_GRAVIDITY,
+  CURRENT_PARITY,
+  ID,
+  LOCATION,
+  PREVIOUS_PREGNANCY_RISK,
+} from '../../constants';
 import './index.css';
 
 interface Props {
@@ -15,27 +24,27 @@ export default function BasicInformation(props: Props) {
   return (
     <Row id={'detailsRow'}>
       <Card id={'detailsCard'}>
-        <CardTitle>Basic Information</CardTitle>
+        <CardTitle>{BASIC_INFORMATION}</CardTitle>
         <Row>
           <Col className={'detailsColumn'} sm="12" lg="6">
-            <span className={'right'}>ID</span>
+            <span className={'right'}>{ID}</span>
             <span />
             <span className={'left'}>{props.id}</span>
-            <span className={'right'}>Current Gravidity</span>
+            <span className={'right'}>{CURRENT_GRAVIDITY}</span>
             <span />
             <span className={'left'}>{props.currentGravidity}</span>
-            <span className={'right'}>Current Parity</span>
+            <span className={'right'}>{CURRENT_PARITY}</span>
             <span />
             <span className={'left'}>{props.currentParity}</span>
           </Col>
           <Col className={'detailsColumn'} sm="12" lg="6">
-            <span className={'right'}>Location</span>
+            <span className={'right'}>{LOCATION}</span>
             <span />
             <span className={'left'}>{props.location}</span>
-            <span className={'right'}>Current EDD</span>
+            <span className={'right'}>{CURRENT_EDD}</span>
             <span />
             <span className={'left'}>{props.currentEdd}</span>
-            <span className={'right'}>Previous Pregnancy Risk</span>
+            <span className={'right'}>{PREVIOUS_PREGNANCY_RISK}</span>
             <span />
             <span className={'left'}>{props.previousPregnancyRisk}</span>
           </Col>
