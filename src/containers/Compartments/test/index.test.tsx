bbccc563224ab996433544d74ed7123a8d0e4eea
FrankApiyo <franklineapiyo@gmail.com>
2019-10-23 14:50:28 +0300

    Add test: ''must display the correct data'

--- src/containers/Compartments/test/index.test.tsx
+++ src/containers/Compartments/test/index.test.tsx
@@ -1,5 +1,6 @@
 import reducerRegistry from '@onaio/redux-reducer-registry';
 import { mount, shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import { createBrowserHistory } from 'history';
 import React from 'react';
 import { Provider } from 'react-redux';
@@ -7,6 +8,7 @@ import { Router } from 'react-router';
 import ConnectedCompartments from '..';
 import reducer, { fetchSms, reducerName } from '../../../store/ducks/sms_events';
 import store from '../../../store/index';
+import { smsDataFixtures } from './fixtures';
 
 const history = createBrowserHistory();
 reducerRegistry.register(reducerName, reducer);
@@ -36,14 +38,30 @@ describe('Compartments', () => {
     expect(wrapper.find('Container').find('div')).toHaveLength(16);
 
     // compartment title should have the correct text
-    expect(wrapper.find('#compartment_title')).toMatchSnapshot('compartment title');
+    expect(toJson(wrapper.find('#compartment_title'))).toMatchSnapshot('compartment title');
 
     // the sub-heading right below the title
-    expect(wrapper.find('#breadcrumb')).toMatchSnapshot('subtitle');
+    expect(toJson(wrapper.find('#breadcrumb'))).toMatchSnapshot('subtitle');
 
     // ensure 3 data circle cards are found
     expect(wrapper.find('DataCircleCard')).toHaveLength(3);
 
     expect(wrapper.find('Compartments').find('Row')).toHaveLength(3);
+  });
+
+  it('must display the correct data', () => {
+    store.dispatch(fetchSms(smsDataFixtures));
+    const wrapper = mount(
+      <Provider store={store}>
+        <Router history={history}>
+          <ConnectedCompartments />
+        </Router>
+      </Provider>
+    );
+
+    // the numbers in this snapshot encode the correctness of the private functions in the Compartments component
+    expect(toJson(wrapper.find('Compartments').find('.dataCircleCard.card'))).toMatchSnapshot(
+      'compartments'
+    );
   });
 });
