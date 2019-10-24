5ab9c18feba572bcc610810856b8a29cf66ca543
FrankApiyo <franklineapiyo@gmail.com>
2019-10-24 10:07:46 +0300

    Add test for convertStringArray and getEventsPregnancyArra

--- src/components/ReportTable/test/index.test.tsx
+++ src/components/ReportTable/test/index.test.tsx
@@ -1,7 +1,19 @@
+import { exportAllDeclaration } from '@babel/types';
 import { mount, shallow } from 'enzyme';
 import { createBrowserHistory } from 'history';
 import React from 'react';
-import ReportTable from '..';
+import ReportTable, { convertToStringArray, getEventsPregnancyArray } from '..';
+import { SmsData } from '../../../store/ducks/sms_events';
+import {
+  convertToStringArrayInput,
+  convertToStringArrayInput2,
+  convertToStringArrayOutput,
+  convertToStringArrayOutput2,
+  getEventsPregnancyArrayInput1,
+  getEventsPregnancyArrayInput2,
+  getEventsPregnancyArrayOutput1,
+  getEventsPregnancyArrayOutput2,
+} from './fixtures';
 
 const history = createBrowserHistory();
 
@@ -10,16 +22,32 @@ describe('ReportTable', () => {
     jest.resetAllMocks();
   });
 
-  it('renders without crashing', () => {
+  it('must render without crashing', () => {
     shallow(<ReportTable singlePatientEvents={[]} />);
   });
 
-  it('renders correctly', () => {
+  it('must render correctly', () => {
     // const wrapper = mount(<ReportTable singlePatientEvents={[]} />);
     // expect(toJson(wrapper)).toMatchSnapshot();
     // I have found a blocker here. This throws a scary error that still
     // makes no sense to me.
   });
+});
 
-  // TODO: test the functions in ReportTable
+describe('convertToStringArray() ', () => {
+  it('must return the correct value for the provided input', () => {
+    expect(convertToStringArray(convertToStringArrayInput2)).toEqual(convertToStringArrayOutput2);
+    expect(convertToStringArray(convertToStringArrayInput)).toEqual(convertToStringArrayOutput);
+  });
+});
+
+describe('getEventsPregnancyArray()', () => {
+  it('must return the correct value for the provided input', () => {
+    expect(getEventsPregnancyArray(getEventsPregnancyArrayInput1 as SmsData[])).toEqual(
+      getEventsPregnancyArrayOutput1
+    );
+    expect(getEventsPregnancyArray(getEventsPregnancyArrayInput2)).toEqual(
+      getEventsPregnancyArrayOutput2
+    );
+  });
 });
