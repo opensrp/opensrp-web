6422b30df3442ddb3ec03e7abc4ae214eedb8a5b
Nafiz Ahmed <redoyahmed11@gmail.com>
2019-10-28 13:35:54 +0600

    Register the client reducer to Household Profile

--- src/containers/Households/Profile/index.tsx
+++ src/containers/Households/Profile/index.tsx
@@ -1,18 +1,24 @@
+import reducerRegistry from '@onaio/redux-reducer-registry';
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 import { Store } from 'redux';
+import { OPENSRP_CLIENT_ENDPOINT, OPENSRP_HOUSEHOLD_ENDPOINT } from '../../../configs/env';
 import { OpenSRPService } from '../../../services/opensrp';
-import {
+import clientReducer, {
   fetchClient,
   fetchEvents,
   fetchMembers,
   getClient,
   getEventsArray,
   getMembersArray,
+  reducerName as clientReducerName,
 } from '../../../store/ducks/client';
 import { Event } from '../../../store/ducks/client';
 import { Household } from '../../../store/ducks/clients';
+
+/** register the client reducer */
+reducerRegistry.register(clientReducerName, clientReducer);
 
 /** interface for HouseholdProfile URL params */
 interface HouseholdProfileURLParams {
