c172be0c901e51a5ecf272555c1f4e5b0211e2f1
FrankApiyo <franklineapiyo@gmail.com>
2019-10-29 04:26:57 +0300

    Add table to HierarchichalDataTable

--- src/containers/HierarchichalDataTable/index.tsx
+++ src/containers/HierarchichalDataTable/index.tsx
@@ -14,14 +14,21 @@ import {
   COMMUNE,
   COMPARTMENTS_URL,
   DISTRICT,
+  EDD,
+  GRAVIDITY,
   HIERARCHICAL_DATA_URL,
   HIGH,
   HIGH_RISK,
+  LOCATION,
   LOCATION_SLICES,
   LOW,
   LOW_RISK,
   NO_RISK,
+  PARITY,
+  PATIENT_ID,
+  PREVIOUS_PREGNANCY_RISK,
   PROVINCE,
+  RISK_CATEGORY,
   TOTAL,
   UP,
   VILLAGE,
@@ -45,7 +52,7 @@ interface LocationWithData extends Location {
 }
 
 interface State {
-  data: any;
+  data: LocationWithData[];
   district: string;
 }
 
@@ -448,6 +455,47 @@ class HierarchichalDataTable extends Component<Props, State> {
             </CardBody>
           </Card>
         </Row>
+        {this.props.current_level === 3 ? (
+          <Row>
+            <Table striped={true} borderless={true}>
+              <thead id="header">
+                <tr>
+                  <th className="default-width">{PATIENT_ID}</th>
+                  <th className="default-width">{GRAVIDITY}</th>
+                  <th className="default-width">{PARITY}</th>
+                  <th className="default-width">{LOCATION}</th>
+                  <th className="default-width">{EDD}</th>
+                  <th className="default-width">{PREVIOUS_PREGNANCY_RISK}</th>
+                  <th className="default-width">{RISK_CATEGORY}</th>
+                </tr>
+              </thead>
+              <tbody id="body">
+                {this.props.smsData.length
+                  ? this.props.smsData
+                      .filter((dataItem: SmsData) => {
+                        const locationIds = this.state.data.map(
+                          (location: LocationWithData) => location.location_id
+                        );
+                        return locationIds.includes(dataItem.location_id);
+                      })
+                      .map((dataItem: SmsData) => {
+                        return (
+                          <tr key={dataItem.event_id}>
+                            <td className="default-width">{dataItem.anc_id}</td>
+                            <td className="default-width">{dataItem.gravidity}</td>
+                            <td className="default-width">{dataItem.parity}</td>
+                            <td className="default-width">{dataItem.location_id}</td>
+                            <td className="default-width">{dataItem.lmp_edd}</td>
+                            <td className="default-width">{dataItem.previous_risks}</td>
+                            <td className="default-width">{dataItem.logface_risk}</td>
+                          </tr>
+                        );
+                      })
+                  : null}
+              </tbody>
+            </Table>
+          </Row>
+        ) : null}
       </Container>
     );
   }
