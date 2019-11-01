bb82cdcd0741e57b557aef2c0a362ac65ab1ddab
p-netm <pmuriuki98@gmail.com>
2019-11-01 14:42:13 +0300

    Refactor utility functionalities

--- src/components/FilterCards/index.tsx
+++ src/components/FilterCards/index.tsx
@@ -2,26 +2,23 @@ import moment from 'moment';
 import React, { useEffect, useState } from 'react';
 import { Card, CardBody, Col, Row } from 'reactstrap';
 
-// TODO - see if this can be made an enum
 /** enumerable values for time unit */
-export const periodUnitTuple = [
-  'seconds',
-  'minutes',
-  'hours',
-  'days',
-  'weeks',
-  'months',
-  'years',
-] as const;
-export type PeriodUnitTuple = typeof periodUnitTuple;
-export type PeriodUnitType = PeriodUnitTuple[number];
+export enum TimeUnits {
+  SECONDS = 'seconds',
+  MINUTES = 'minutes',
+  HOURS = 'hours',
+  DAYS = 'days',
+  WEEKS = 'weeks',
+  MONTHS = 'months',
+  YEARS = 'years',
+}
 
 /** Type interface for duration */
 interface DurationType {
   /** amount in integers  */
   howLong: number;
   /** time unit e.g. 'hours', 'minutes' you get it. */
-  unit: PeriodUnitType;
+  unit: TimeUnits;
 }
 
 /** props for FilterCards component */
@@ -31,85 +28,126 @@ export interface Props<T> {
    */
   data: T[];
 
-  /** the string value of the property under which the data should be filtered
+  /** the string value of the property via which the data should be filtered
    * on
    */
   categoryField: string;
 
   /** An array of duration objects; works as a form of pseudo filter
-   * helping zero down further after data has been filtered using above property name
+   * helping zero down further after data has been filtered using above categoryField
    */
   periods: DurationType[];
 
-  /** Proprty field that data will be filtered against for the given periods */
-  etaField: string;
+  /** property name where data[idx].[<timeField>] will represent the values
+   * which will be subject to filtering based on the periods
+   */
+  timeField: string;
 
   /** a union type of the categories that you wish to be subject of the filter ;
-   * This will usually be enumerable values that the property given above can
+   * This will usually be enumerable values that the categoryField given above can
    * have.
    */
   categories: Set<string>;
 }
 
 export const defaultProps: Props<{}> = {
-  categories: new Set(''),
+  categories: new Set(['']),
   categoryField: '',
   data: [],
-  etaField: '',
-  periods: [{ howLong: 1, unit: 'weeks' }, { howLong: 2, unit: 'weeks' }],
+  periods: [{ howLong: 1, unit: TimeUnits.WEEKS }, { howLong: 2, unit: TimeUnits.WEEKS }],
+  timeField: '',
 };
 
-interface GroupedFilters<T>{
-  [key: string]: T[];
+/** interface for how filtered data will be passed back to the calling
+ * component or to a display component via render props
+ */
+interface GroupedFilters<T> {
+  /** chunk of original data that is now currently filtered subject to
+   * info in the meta
+   */
+  filteredData: T[];
+  /** meta information regarding the filter applied to above data */
+  meta: {
+    /** describes the numeric part of the period under which the filtered
+     * data was filtered
+     */
+    howLong: number;
+
+    /** The category value */
+
+    category: string;
+    /** describes the unit part of the period under which the filtered
+     * data was filtered
+     */
+    unit: string;
+  };
 }
 
 /**  track category + period  in state so as to know what card to be active */
 
 function FilterCards<T>(props: Props<T>) {
-  const {periods, data, etaField, categoryField, categories} = props
-  const [groupedFilters, setGroupedFilters] = useState<GroupedFilters<T>>({})
+  const { periods, data, timeField, categoryField, categories } = props;
+  const [groupedFilters, setGroupedFilters] = useState<GroupedFilters<T>>({});
 
-  useEffect(() => {
-    
-  })
-  return (
-    <Row>
-      {({render(groupFilterData<T>())})}
-    </Row>
-  );
-};
+  useEffect(() => {});
+  return <Row>{/* {({render(groupFilterData<T>())})} */}</Row>;
+}
 
-/** Create a dictionary where {category: number of records} */
-function groupFilterData<T>(
+/** creates an object showing how the data was filtered
+ * based on the periods and the category field passed in as props
+ * @param {DurationType} periods - filter based on this periods
+ * @param {data} {T[]} - the original unfiltered data
+ * @param {categories} - Filter data with respect to this categories
+ * @param {string} categoryField - Filter data with respect to above categories for this property name
+ * @param {string} timeField - property name for field that holds the eta data for each data obj.
+ */
+export function groupFilterData<T>(
   periods: DurationType[],
   data: T[],
   categories: Set<string>,
   categoryField: string,
-  etaField: string
-): GroupedFilters<T> {
-  const dictionary: GroupedFilters<T> = {};
+  timeField: string
+): Array<GroupedFilters<T>> {
+  const filteredData: Array<GroupedFilters<T>> = [];
   // TODO - the period is in the form given by the server, need to parse that in an identifible format.
   periods.forEach(period => {
     categories.forEach(category => {
-      dictionary[`${period.howLong}-${period.unit}-${category}`] = data.filter(
-        entry =>
-          (entry as any)[categoryField] === category && isInPeriod<T>(entry, period, etaField)
-      );
+      filteredData.push({
+        filteredData: data.filter(
+          entry =>
+            (entry as any)[categoryField] === category && isInPeriod<T>(entry, period, timeField)
+        ),
+        meta: {
+          category,
+          ...period,
+        },
+      });
     });
   });
-  return dictionary
+  return filteredData;
 }
 
-/** returns boolean showing whether the data object etaField's value
+/** returns boolean showing whether the data object timeField's value
  * is in between the Date.now through to Date.now + period
+ * @param {T} dataObj - object describing event
+ * @param {DurationType} period - data object can be within or without now to end of given period
+ * @param {string} timeField - dataObj[timeField]'s value shows when dataObj event will be done
+ * @param {string | null} startPeriod - changes the point from which we decide the period starts
  */
-function isInPeriod<T>(dataObj: T, period: DurationType, etaField: string): boolean {
-  const now = new moment();
-  const eta = new moment((dataObj as any)[etaField])
-  const inPeriod = moment.duration(period.howLong, period.unit)
-  // from now
-  const durationToPeriodEnd = moment.duration(now.diff(eta))
+export function isInPeriod<T>(
+  dataObj: T,
+  period: DurationType,
+  timeField: string,
+  startPeriod: string | null = null
+): boolean {
+  const now = moment(startPeriod ? startPeriod : {});
+  const dataObjEta = moment((dataObj as any)[timeField]);
+  /** duration to end of period */
+  const periodBounds = moment.duration(period.howLong, period.unit);
+  /** eta to the event described by data object */
+  const timeToDataObjEta = moment.duration(dataObjEta.diff(now));
 
-  return inPeriod <= durationToPeriodEnd
+  // if timeToDataObjEta is less that the periodBound duration, means
+  // DataObj event is within the given period otherwise without
+  return timeToDataObjEta <= periodBounds;
 }
-
