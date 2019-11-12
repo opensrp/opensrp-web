fe7eaa19556c5a5c3e24a7c0781c38d368937c62
Kipchirchir Sigei <arapgodsmack@gmail.com>
2019-11-12 17:01:38 +0300

    Safely mount/unmount chart component
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- src/components/MotherWeightChart/index.tsx
+++ src/components/MotherWeightChart/index.tsx
@@ -1,7 +1,9 @@
 import * as Highcharts from 'highcharts';
 import * as React from 'react';
 import { Card, CardTitle } from 'reactstrap';
+import { clearTimeout, setTimeout } from 'timers';
 import { MOTHER_WEIGHT_TRACKING } from '../../constants';
+import { FlexObject } from '../../helpers/utils';
 import './index.css';
 
 interface Props {
@@ -23,100 +25,121 @@ export default class MotherWeightChart extends React.Component<Props, State> {
       };
     }
   }
+  public static calcChartWidth(chart: any) {
+    if (chart && chart.series) {
+      chart.setSize(0.8 * window.innerWidth, null);
+      return chart;
+    }
+  }
+
   constructor(props: Props) {
     super(props);
     // Init state.
-    this.state = { chart: {} };
+    this.state = { chart: null };
   }
-  public componentDidMount() {
-    const chart = Highcharts.chart('chart-wrapper', {
-      chart: {
-        type: 'line',
-        width: 0.8 * window.innerWidth,
-      },
-      legend: {
-        align: 'right',
-        layout: 'vertical',
-        verticalAlign: 'middle',
-      },
-      title: {
-        text: '',
-      },
-      // tslint:disable-next-line: object-literal-sort-keys
-      tooltip: {
-        backgroundColor: 'white',
-        borderColor: '#DADCE0',
-        borderRadius: 10,
-        borderWidth: 1,
-        shadow: {
-          color: '#D7D7E0',
-          offsetX: 0,
-          offsetY: 2,
-          opacity: 0.2,
-          width: 8,
-        },
-        formatter() {
-          return `Mother's weight - ${this.x}<br> weight <b>${this.y}</b>`;
-        },
-      },
 
-      subtitle: {
-        text: undefined,
-      },
-
-      yAxis: {
+  public componentDidMount(): void {
+    let chart: any;
+    const self: FlexObject = this;
+    if (self.state.chart) {
+      self.state.chart.destroy();
+    }
+    self.timeout = setTimeout(() => {
+      chart = Highcharts.chart('chart-wrapper', {
+        chart: {
+          type: 'line',
+          width: 0.8 * window.innerWidth,
+        },
+        legend: {
+          align: 'right',
+          layout: 'vertical',
+          verticalAlign: 'middle',
+        },
         title: {
           text: '',
         },
-      },
-
-      xAxis: {
-        categories: [
-          'Jan',
-          'Feb',
-          'Mar',
-          'Apr',
-          'May',
-          'Jun',
-          'Jul',
-          'Aug',
-          'Sep',
-          'Oct',
-          'Nov',
-          'Dec',
-        ],
-      },
-
-      series: [
-        {
-          data: this.props.weights,
-          name: 'weight',
+        // tslint:disable-next-line: object-literal-sort-keys
+        tooltip: {
+          backgroundColor: 'white',
+          borderColor: '#DADCE0',
+          borderRadius: 10,
+          borderWidth: 1,
+          shadow: {
+            color: '#D7D7E0',
+            offsetX: 0,
+            offsetY: 2,
+            opacity: 0.2,
+            width: 8,
+          },
+          formatter() {
+            return `Mother's weight - ${this.x}<br> weight <b>${this.y}</b>`;
+          },
         },
-      ] as any,
 
-      responsive: {
-        rules: [
+        subtitle: {
+          text: undefined,
+        },
+
+        yAxis: {
+          title: {
+            text: '',
+          },
+        },
+
+        xAxis: {
+          categories: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec',
+          ],
+        },
+
+        series: [
           {
-            chartOptions: {
-              legend: {
-                align: 'center',
-                layout: 'horizontal',
-                verticalAlign: 'bottom',
+            data: this.props.weights,
+            name: 'weight',
+          },
+        ] as any,
+
+        responsive: {
+          rules: [
+            {
+              chartOptions: {
+                legend: {
+                  align: 'center',
+                  layout: 'horizontal',
+                  verticalAlign: 'bottom',
+                },
+              },
+              condition: {
+                maxWidth: 5000,
               },
             },
-            condition: {
-              maxWidth: 5000,
-            },
-          },
-        ],
-      },
-    });
-    window.addEventListener('resize', () => this.calcChartWidth(chart));
+          ],
+        },
+      });
+    }, 300);
+    window.addEventListener('resize', () => MotherWeightChart.calcChartWidth(chart));
     this.setState({ chart });
   }
 
-  public componentWillUnmount() {
-    window.removeEventListener('resize', () => this.calcChartWidth(this.state.chart));
+  public componentWillUnmount(): void {
+    const self: FlexObject = this;
+    if (self.state.chart) {
+      window.removeEventListener('resize', () =>
+        MotherWeightChart.calcChartWidth(this.state.chart)
+      );
+      clearTimeout(self.timeout);
+    }
   }
 
   public render() {
@@ -127,11 +150,4 @@ export default class MotherWeightChart extends React.Component<Props, State> {
       </Card>
     );
   }
-
-  private calcChartWidth = (chart: any) => {
-    if (chart) {
-      chart.setSize(0.8 * window.innerWidth, null);
-      this.setState({ chart });
-    }
-  };
 }
