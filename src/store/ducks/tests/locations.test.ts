f24c5f78e1e3dda4e47f48dac03519f558054ced
Kipchirchir Sigei <arapgodsmack@gmail.com>
2019-11-14 15:38:11 +0300

    add tests for user location reducer
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- src/store/ducks/tests/locations.test.ts
+++ src/store/ducks/tests/locations.test.ts
@@ -1,11 +1,17 @@
 import reducerRegistry from '@onaio/redux-reducer-registry';
 import { COMMUNE, DISTRICT, PROVINCE, VILLAGE } from '../../../constants';
-import { districts, provinces } from '../../../containers/HierarchichalDataTable/test/fixtures';
+import {
+  districts,
+  provinces,
+  userLocationDetails,
+} from '../../../containers/HierarchichalDataTable/test/fixtures';
 import store from '../../index';
 import reducer, {
   fetchLocations,
+  fetchUserLocations,
   getLocations,
   getLocationsOfLevel,
+  getUserLocations,
   reducerName,
   removeLocations,
 } from '../locations';
@@ -14,7 +20,8 @@ reducerRegistry.register(reducerName, reducer);
 
 describe('reducers/location', () => {
   it('should have initial state', () => {
-    expect(getLocations(store.getState())).toEqual([]);
+    expect(getLocations(store.getState())).toHaveLength(0);
+    expect(getUserLocations(store.getState())).toHaveLength(0);
     expect(getLocationsOfLevel(store.getState(), PROVINCE)).toEqual([]);
     expect(getLocationsOfLevel(store.getState(), DISTRICT)).toEqual([]);
     expect(getLocationsOfLevel(store.getState(), COMMUNE)).toEqual([]);
@@ -25,6 +32,11 @@ describe('reducers/location', () => {
     store.dispatch(fetchLocations(provinces));
     expect(getLocationsOfLevel(store.getState(), PROVINCE)).toEqual(provinces);
     expect(getLocations(store.getState())).toEqual(provinces);
+  });
+
+  it('Fetch user location details action creator works correctly', () => {
+    store.dispatch(fetchUserLocations(userLocationDetails));
+    expect(getUserLocations(store.getState())).toEqual(userLocationDetails);
   });
 
   it('remove locations actions works correctly', () => {
