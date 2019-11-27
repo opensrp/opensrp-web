b4aa7387ca9b3a56d41e4932affde1e38726c8b8
p-netm <pmuriuki98@gmail.com>
2019-11-27 16:19:22 +0300

    Remove code for react-async

--- src/containers/ANC/list/dataLoading.tsx
+++ src/containers/ANC/list/dataLoading.tsx
@@ -3,14 +3,15 @@ import { OpenSRPService } from '../../../services/opensrp';
 import store from '../../../store';
 import { Client } from '../../../store/ducks/clients';
 
-/** Queries opensrp server for single page of anc records
+/** Queries OpenSRP server for single page of anc records
  * @param {typeof OpenSRPService} service -  the OpenSRP service
+ * @param {typeof } fetchClientsCreator -  the action creator
  */
-export const loadANCList = async (
-  { service }: any,
-  { signal }: AbortController
-): Promise<Client[]> => {
+export const loadANCList = async (service: typeof OpenSRPService, fetchClientsCreator: any) => {
   const serve = new service(OPENSRP_CLIENT_ENDPOINT);
   const filterParams = {};
-  return await serve.list(filterParams);
+  return await serve
+    .list(filterParams)
+    .then((response: Client[]) => store.dispatch(fetchClientsCreator(response)))
+    .catch((err: Error) => alert(err.message));
 };
--- src/containers/ANC/list/index.tsx
+++ src/containers/ANC/list/index.tsx
@@ -1,43 +1,60 @@
 /** Presentational component and container for the ANC listing page */
 import React from 'react';
-import { useAsync } from 'react-async';
+import { connect } from 'react-redux';
+import Loading from '../../../components/page/Loading';
 import { OpenSRPService } from '../../../services/opensrp';
-import store from '../../../store';
+import { Client, fetchClients } from '../../../store/ducks/clients';
 import { loadANCList } from './dataLoading';
 import { useFilters } from './hooks';
 
 interface ANCListProps {
+  ANCList: Client[];
   fetchClientsCreator: any;
   service: typeof OpenSRPService;
 }
 
 const defaultANCListProps: ANCListProps = {
+  ANCList: [],
   fetchClientsCreator: (f: any) => f,
   service: OpenSRPService,
 };
 
 /** dumb component responsible for showing ANC listings */
 const ANCListView: React.FC<ANCListProps> = props => {
-  const { service, fetchClientsCreator } = props;
+  const { service, fetchClientsCreator, ANCList } = props;
   const [filterState, addFilter, setFilterState] = useFilters();
-  const { data, error, isPending } = useAsync({ promiseFn: loadANCList, service });
-  if (isPending) {
-    return <h1>Loading</h1>;
+
+  React.useEffect(() => {
+    // make api call to get all anc list data on component mount
+    loadANCList(service, fetchClientsCreator);
+  });
+
+  if (ANCList.length < 1) {
+    return <Loading />;
   }
-  if (error) {
-    return <h1>Something went wrong</h1>;
-  }
-  if (data) {
-    store.dispatch(fetchClientsCreator(data));
-    return <h1>Start rendering table</h1>;
-  }
-  return null;
+
+  return <div />;
 };
 
 ANCListView.defaultProps = defaultANCListProps;
 
 // create container
+interface DispatchedStateToProps {
+  ANCList: Client[];
+}
+const mapStateToProps = (): DispatchedStateToProps => {
+  return {
+    ANCList: [], // getANCArray;
+  };
+};
 
-const ConnectedANCListView = ANCListView;
+const mapDispatchToProps = {
+  fetchClientsCreator: fetchClients,
+};
+
+const ConnectedANCListView = connect(
+  mapStateToProps,
+  mapStateToProps
+)(ANCListView);
 
 export default ConnectedANCListView;
