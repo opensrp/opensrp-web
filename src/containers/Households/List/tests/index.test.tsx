e78f28f3d57c7dccb7a413a7f741505ec10644c7
Prodhan <02nahid02@gmail.com>
2019-11-28 15:45:31 +0600

    Add test code for household list

---
+++ src/containers/Households/List/tests/index.test.tsx
@@ -0,0 +1,51 @@
+import reducerRegistry from '@onaio/redux-reducer-registry';
+import { mount, shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import * as React from 'react';
+import { Provider } from 'react-redux';
+import store from '../../../../store';
+import householdsReducer, {
+  reducerName as householdsReducerName,
+} from '../../../../store/ducks/households';
+import * as fixtures from '../../../../store/ducks/tests/fixtures';
+import { HouseholdList, HouseholdListProps } from '../../List/index';
+
+reducerRegistry.register(householdsReducerName, householdsReducer);
+
+jest.mock('../../../../configs/env');
+describe('containers/households/list/Householdlist', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('renders without crashing', () => {
+    const props: HouseholdListProps = {
+      fetchHouseholdsActionCreator: jest.fn(),
+      householdsArray: [],
+      opensrpService: jest.fn(),
+      removeHouseholdsActionCreator: jest.fn(),
+      setTotalRecordsActionCreator: jest.fn(),
+      totalRecordsCount: 0,
+    };
+    shallow(<HouseholdList {...props} />);
+  });
+
+  it('works correctly with the redux store', () => {
+    const props: HouseholdListProps = {
+      fetchHouseholdsActionCreator: jest.fn(),
+      householdsArray: [],
+      opensrpService: jest.fn(),
+      removeHouseholdsActionCreator: jest.fn(),
+      setTotalRecordsActionCreator: jest.fn(),
+      totalRecordsCount: 0,
+    };
+    const wrapper = mount(
+      <Provider store={store}>
+        <HouseholdList {...props} />
+        );
+      </Provider>
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+    wrapper.unmount();
+  });
+});
