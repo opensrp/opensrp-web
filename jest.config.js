6a270c18fe55e51733d103ff81d69efc9ace601f
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-12-02 19:11:16 +0300

    Add jest config

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'packages/**/*.{js,jsx,ts,tsx}',
+        '!**/node_modules/**',
+        '!**/coverage/**',
+        '!**/dist/**',
+        '!**/{tests,__tests__}/**',
+    ],
+    setupFiles: ['./setupJest.ts', './setupTests'],
+    roots: ['packages/'],
+    moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            '<rootDir>/__mocks__/fileMock.js',
+        '\\.(css|less)$': 'identity-obj-proxy',
+    },
+};
