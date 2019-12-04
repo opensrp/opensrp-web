f24a0c3c73817f37c43d2e155fd5bb0d02fe781c
p-netm <pmuriuki98@gmail.com>
2019-12-04 16:32:41 +0300

    Test callback is invoked correctly

--- src/components/page/SearchBox/tests/SearchBox.test.tsx
+++ src/components/page/SearchBox/tests/SearchBox.test.tsx
@@ -1,22 +1,44 @@
 import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import React from 'react';
 import SearchBox, { SearchBoxProps } from '../SearchBox';
 
+const callBackMock = jest.fn();
 const props: SearchBoxProps = {
   placeholder: 'Search Household',
-  searchCallBack: jest.fn(),
+  searchCallBack: callBackMock,
 };
 
 describe('components/page/Searchbox', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
   it('renders without crashing', () => {
     const component = shallow(<SearchBox {...props} />);
     const wrapper = component.find('#custom-search-input');
+    expect(toJson(wrapper.find('.search-bar-input'))).toMatchSnapshot('SearchBox input');
+    expect(toJson(wrapper.find('.search-bar-icon'))).toMatchSnapshot('SearchBox ion');
     expect(wrapper.length).toBe(1);
   });
 
-  it('has proptypes and checks the prop values', () => {
-    const component = shallow(<SearchBox {...props} />);
-    const wrapper = component.find('.search-bar-input');
-    expect(wrapper.props().placeholder).toEqual(props.placeholder);
+  it('invokes callback correctly', () => {
+    const wrapper = shallow(<SearchBox {...props} />);
+
+    // simulate typing some text
+    const inputWrapper = wrapper.find('.search-bar-input');
+    expect(inputWrapper.length).toEqual(1);
+
+    inputWrapper.simulate('change', { target: { value: 'randomString' } });
+
+    // callback is not yet called;
+    expect(callBackMock).not.toHaveBeenCalled();
+
+    // simulate click on enter key
+    inputWrapper.simulate('keypress', { key: 'Enter' });
+
+    // now callback should have been called with "'randomString'"
+    expect(callBackMock).toHaveBeenCalledTimes(1);
+    expect(callBackMock).toHaveBeenCalledWith('randomString');
   });
 });
