8cfef8bb0ebfdd798d803574e2e9a5b41c1056aa
p-netm <pmuriuki98@gmail.com>
2019-12-04 16:57:43 +0300

    add test conditions to achieve branch coverage

--- src/components/page/SearchBox/tests/index.test.tsx
+++ src/components/page/SearchBox/tests/index.test.tsx
@@ -3,18 +3,17 @@ import toJson from 'enzyme-to-json';
 import React from 'react';
 import SearchBox, { SearchBoxProps } from '..';
 
-const callBackMock = jest.fn();
-const props: SearchBoxProps = {
-  placeholder: 'Search Household',
-  searchCallBack: callBackMock,
-};
-
 describe('components/page/Searchbox', () => {
   beforeEach(() => {
     jest.resetAllMocks();
   });
 
   it('renders without crashing', () => {
+    const callBackMock = jest.fn();
+    const props: SearchBoxProps = {
+      placeholder: 'Search Household',
+      searchCallBack: callBackMock,
+    };
     const component = shallow(<SearchBox {...props} />);
     const wrapper = component.find('#custom-search-input');
     expect(toJson(wrapper.find('.search-bar-input'))).toMatchSnapshot('SearchBox input');
@@ -23,6 +22,10 @@ describe('components/page/Searchbox', () => {
   });
 
   it('invokes callback correctly', () => {
+    const callBackMock = jest.fn();
+    const props: SearchBoxProps = {
+      searchCallBack: callBackMock,
+    };
     const wrapper = shallow(<SearchBox {...props} />);
 
     // simulate typing some text
@@ -33,6 +36,11 @@ describe('components/page/Searchbox', () => {
 
     // callback is not yet called;
     expect(callBackMock).not.toHaveBeenCalled();
+
+    // simulate click on placebo key: other than enter: the space bar
+    inputWrapper.simulate('keypress', { key: '' });
+    // callback should not have been called yet
+    expect(callBackMock).toHaveBeenCalledTimes(0);
 
     // simulate click on enter key
     inputWrapper.simulate('keypress', { key: 'Enter' });
