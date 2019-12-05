caa25438c13dd4d9df3528eca82240d876681e4f
p-netm <pmuriuki98@gmail.com>
2019-12-05 12:13:11 +0300

    Add a few more tests

--- src/store/ducks/tests/events.test.ts
+++ src/store/ducks/tests/events.test.ts
@@ -6,6 +6,7 @@ import eventReducer, {
   getEventsArray,
   getEventsById,
   reducerName as eventReducerName,
+  removeEventsAction,
 } from '../events';
 
 import * as fixtures from '../tests/fixtures';
@@ -15,6 +16,7 @@ reducerRegistry.register(eventReducerName, eventReducer);
 describe('reducers/clients', () => {
   beforeEach(() => {
     jest.resetAllMocks();
+    store.dispatch(removeEventsAction);
   });
 
   it('events selectors work for empty initialState', () => {
@@ -22,16 +24,40 @@ describe('reducers/clients', () => {
     expect(getEventsArray(store.getState())).toEqual([]);
   });
 
-  it('fetches events correctly', () => {
+  it('selectors work correctly on non-empty state', () => {
+    // also how reducer handles actions on empty state.
     store.dispatch(fetchEvents(fixtures.events));
-    expect(getEventsArray(store.getState())).toEqual(values(fixtures.events));
+    expect(getEventsById(store.getState())).toEqual({
+      [fixtures.event1.baseEntityId]: fixtures.event1,
+      [fixtures.event2.baseEntityId]: fixtures.event2,
+      [fixtures.event3.baseEntityId]: fixtures.event3,
+    });
+    expect(getEventsArray(store.getState())).toEqual(fixtures.events);
   });
 
-  it('fetches events correctly', () => {
+  it('reducer handles unknown types graciously', () => {
+    const initialState = store.getState();
+    store.dispatch({
+      ...fetchEvents(fixtures.events),
+      type: 'UnknownType',
+    });
+    expect(store.getState()).toEqual(initialState);
+  });
+
+  it('fetch events works correctly on non-empty state', () => {
+    store.dispatch(fetchEvents([fixtures.event1]));
+    expect(getEventsArray(store.getState().length)).toEqual(1);
+    store.dispatch(fetchEvents([fixtures.event2]));
+    expect(getEventsArray(store.getState()).length).toEqual(2);
+  });
+
+  it('remove events action works', () => {
     store.dispatch(fetchEvents([fixtures.event1, fixtures.event2]));
     expect(getEventsById(store.getState())).toEqual({
       '5e8b72c5-0b36-9d7d-829f-279d1482c96a': fixtures.event2,
       '6e8b72c5-0b36-4e46-829f-8ujd1482c96a': fixtures.event1,
     });
+    store.dispatch(removeEventsAction);
+    expect(getEventsArray(store.getState())).toEqual([]);
   });
 });
