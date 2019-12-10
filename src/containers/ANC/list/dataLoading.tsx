5621b55fb90617b319e1eac6cb9a6d2d34b6d9b9
p-netm <pmuriuki98@gmail.com>
2019-12-10 18:59:25 +0300

    Cleanup; add docstrings & refactor types.

--- src/containers/ANC/list/dataLoading.tsx
+++ src/containers/ANC/list/dataLoading.tsx
@@ -1,15 +1,16 @@
+import { PromiseFn } from 'react-async';
 import { OPENSRP_CLIENT_ENDPOINT } from '../../../configs/env';
-import { OpenSRPService } from '../../../services/opensrp';
 import store from '../../../store';
 import { Client, fetchClients } from '../../../store/ducks/clients';
 
-/** Queries OpenSRP server for single page of anc records
- * @param {typeof OpenSRPService} service -  the OpenSRP service
- * @param {typeof } fetchClientsCreator -  the action creator
+/** Queries OpenSRP server for single page of anc records its arguments are
+ * are de-structured from the options passed into the useAsync hook
+ * @param {typeof OpenSRPService} service- the openSRP service
+ * @param {any} filterParams - parametrize the get request
+ * @param {AbortController} signal - used to abort promises
  */
-export const loadANCList = async ({ service }: any, { signal }: any) => {
+export const loadANCList: PromiseFn<Client[]> = async ({ service, filterParams }, { signal }) => {
   const serve = new service(OPENSRP_CLIENT_ENDPOINT);
-  const filterParams = {};
   return await serve.list(filterParams).then((response: Client[]) => {
     store.dispatch(fetchClients(response));
     return response;
