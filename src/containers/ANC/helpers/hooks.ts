3b3cb8f41078e2c57c2d76a167eb8fd63adef641
p-netm <pmuriuki98@gmail.com>
2019-12-15 22:09:09 +0000

    Add useParamFilter hook

---
+++ src/containers/ANC/helpers/hooks.ts
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+/** describes how filter params will be stored */
+interface FilterParamState {
+  [key: string]: string;
+}
+
+/** interface for dataObj handled by setFilterParam */
+type FilterStateAction = FilterParamState;
+
+/** hook to manage filter parameter's state; i.e
+ * parameters that will be added to the url as query string
+ */
+export function useParamFilters() {
+  const [filterParams, setFilterFilterParams] = useState<FilterParamState>({});
+
+  /** filter params will be added to those already existing
+   * @param {FilterStateAction} params - the filter param to add to state
+   */
+  const appendFilters = (params: FilterStateAction) =>
+    setFilterFilterParams({ ...filterParams, ...params });
+
+  /** repopulates the state with new filter params */
+  const resetFilters = (params: FilterStateAction) => setFilterFilterParams({ ...params });
+
+  return { filterParams, setFilterFilterParams, appendFilters, resetFilters };
+}
