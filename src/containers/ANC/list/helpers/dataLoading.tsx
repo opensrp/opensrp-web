e5848bae35a22971329d2e4a8068099a7e09ac07
p-netm <pmuriuki98@gmail.com>
2019-12-17 12:15:36 +0300

    Refactor LoadANCList helper func

--- src/containers/ANC/list/helpers/dataLoading.tsx
+++ src/containers/ANC/list/helpers/dataLoading.tsx
@@ -6,13 +6,18 @@ import { Client, fetchClients } from '../../../../store/ducks/clients';
 /** Queries OpenSRP server for single page of anc records its arguments are
  * are de-structured from the options passed into the useAsync hook
  * @param {typeof OpenSRPService} service- the openSRP service
- * @param {any} filterParams - parametrize the get request
+ * @param {FlexObject} filterParams - parametrize the get request
  * @param {AbortController} signal - used to abort promises
+ * @param {typeof fetchClients} - action creator
  */
-export const loadANCList: PromiseFn<Client[]> = async ({ service, filterParams }, { signal }) => {
+export const loadANCList: PromiseFn<Client[]> = async (
+  { service, filterParams },
+  { signal }: AbortController,
+  fetchClientsCreator: typeof fetchClients = fetchClients
+) => {
   const serve = new service(OPENSRP_CLIENT_ENDPOINT);
   return await serve.list(filterParams).then((response: Client[]) => {
-    store.dispatch(fetchClients(response));
+    store.dispatch(fetchClientsCreator(response));
     return response;
   });
 };
