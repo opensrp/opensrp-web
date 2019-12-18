a73076d96b9572f79a93fa4e04569f5027a8fb64
p-netm <pmuriuki98@gmail.com>
2019-12-18 19:33:48 +0300

    Test component calls render prop correctly

--- src/components/FilterCardsCategorizer/tests/index.test.tsx
+++ src/components/FilterCardsCategorizer/tests/index.test.tsx
@@ -1,4 +1,7 @@
-import { DurationType, groupFilterData, isInPeriod, TimeUnit } from '..';
+import { mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import React from 'react';
+import { DurationType, FilterCardsCategorizer, groupFilterData, isInPeriod, TimeUnit } from '..';
 import { filterTestData } from './fixtures';
 
 // TODO - understand this; document this
@@ -98,5 +101,64 @@ describe('src/components/FilterCards.utils', () => {
     };
     const result = isInPeriod(dataObj, period, 'edd');
     expect(result).toBeTruthy();
+  });
+});
+
+describe('src/components/filterCardsCategorizer.FilterCardsCategorizer', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+  it('renders correctly', () => {
+    // tslint:disable-next-line: jsx-no-lambda
+    const wrapper = mount(<FilterCardsCategorizer renderCard={f => <div className="dummy" />} />);
+    expect(toJson(wrapper.find('.dummy'))).toMatchInlineSnapshot(`
+      <div
+        className="dummy"
+      />
+    `);
+  });
+
+  it('calls renderCard props with correct arguments', () => {
+    const renderMock = jest.fn(() => <div className="dummy" />);
+    const period: DurationType = {
+      timeLength: 2,
+      timeUnit: TimeUnit.WEEK,
+    };
+    const props = {
+      accessor: 'risk',
+      categories: new Set(['HighRisk', 'LowRisk']),
+      data: filterTestData,
+      parentFilters: [],
+      periods: [period],
+      renderCard: renderMock,
+      timeAccessor: 'edd',
+    };
+    const wrapper = mount(<FilterCardsCategorizer {...props} />);
+    expect(renderMock).toHaveBeenCalled();
+
+    const expected = [
+      {
+        filterFunction: expect.anything(),
+        filterId: 'FILTER_CARDS',
+        filteredData: [
+          { edd: 1573903123481, id: '1', risk: 'HighRisk' },
+          { edd: 1573471123481, id: '2', risk: 'HighRisk' },
+        ],
+        meta: { category: 'HighRisk', timeLength: 2, timeUnit: 'week' },
+        parentFilters: [],
+      },
+      {
+        filterFunction: expect.anything(),
+        filterId: 'FILTER_CARDS',
+        filteredData: [
+          { edd: 1573903123481, id: '3', risk: 'LowRisk' },
+          { edd: 1573809027171, id: '4', risk: 'LowRisk' },
+        ],
+        meta: { category: 'LowRisk', timeLength: 2, timeUnit: 'week' },
+        parentFilters: [],
+      },
+    ];
+
+    expect((renderMock.mock.calls[1] as any)[0]).toMatchObject(expected);
   });
 });
