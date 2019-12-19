ae9d0f2d0d8d18131743e7e4d2d0a491bd718636
p-netm <pmuriuki98@gmail.com>
2019-12-19 19:07:03 +0300

    Fix tests for anc list page

--- src/containers/pages/ANC/list/tests/index.test.tsx
+++ src/containers/pages/ANC/list/tests/index.test.tsx
@@ -1,17 +1,24 @@
 import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
+import { createBrowserHistory } from 'history';
 import React from 'react';
+import { act } from 'react-dom/test-utils';
 import { Helmet } from 'react-helmet';
-import ConnectedANCListView, { ANCListProps, ANCListView } from '../../list';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router';
+import { ANC } from '../../../../../constants';
 import { OpenSRPService } from '../../../../../services/opensrp';
 import store from '../../../../../store';
 import * as clientDux from '../../../../../store/ducks/clients';
-import { allANC } from './fixtures';
+import ConnectedANCListView, { ANCListProps, ANCListView } from '../../list';
+import { allANC, SelectorState } from './fixtures';
 
 // tslint:disable-next-line: no-var-requires
 const fetch = require('jest-fetch-mock');
 
 const signal = new AbortController().signal;
+
+const history = createBrowserHistory();
 
 const props: ANCListProps = {
   ANCArray: [],
@@ -23,6 +30,7 @@ describe('src/containers/pages/ANC', () => {
   beforeEach(() => {
     jest.resetAllMocks();
     fetch.resetMocks();
+    store.dispatch(clientDux.removeClientsAction);
   });
 
   it('renders without crashing', async () => {
@@ -33,24 +41,32 @@ describe('src/containers/pages/ANC', () => {
 
   it('renders correctly', async () => {
     fetch.once(JSON.stringify([]));
-    // unresolved fetch request.
+
     const wrapper = mount(<ANCListView {...props} />);
     expect(wrapper.find('ANCTable').length).toEqual(0);
     expect(wrapper.find('Ripple').length).toEqual(1);
 
     // resolve fetch
     await new Promise(resolve => setImmediate(resolve));
+
+    wrapper.update();
     expect(wrapper.find('ANCTable').length).toEqual(1);
 
-    // we are setting title
+    // are we setting title
     const helmet = Helmet.peek();
-    expect(helmet.title).toEqual(``);
+    expect(helmet.title).toEqual(ANC);
   });
 
   it('works correctly with store', async () => {
     fetch.once(JSON.stringify([]));
     store.dispatch(clientDux.fetchClients(allANC));
-    const wrapper = mount(<ConnectedANCListView {...props} />);
+    const wrapper = mount(
+      <Provider store={store}>
+        <Router history={history}>
+          <ConnectedANCListView {...props} />
+        </Router>
+      </Provider>
+    );
 
     await new Promise(resolve => setImmediate(resolve));
 
@@ -60,10 +76,26 @@ describe('src/containers/pages/ANC', () => {
 
   it('makes the correct api calls', async () => {
     fetch.once(JSON.stringify(allANC));
-    const wrapper = mount(<ConnectedANCListView {...props} />);
+    const wrapper = mount(
+      <Provider store={store}>
+        <Router history={history}>
+          <ConnectedANCListView {...props} />
+        </Router>
+      </Provider>
+    );
 
     await new Promise(resolve => setImmediate(resolve));
-    expect(fetch.mock.calls[0]).toEqual([]);
+    expect(fetch.mock.calls[0]).toEqual([
+      'https://reveal-stage.smartregister.org/opensrp/rest/client/search',
+      {
+        headers: {
+          accept: 'application/json',
+          authorization: 'Bearer null',
+          'content-type': 'application/json;charset=UTF-8',
+        },
+        method: 'GET',
+      },
+    ]);
   });
 
   it('renders correctly on error', async () => {
@@ -75,20 +107,33 @@ describe('src/containers/pages/ANC', () => {
 
     // resolve fetch
     await new Promise(resolve => setImmediate(resolve));
+    wrapper.update();
+
+    expect(toJson(wrapper.find('h2'))).toMatchInlineSnapshot(`
+      <h2>
+        Any Error
+      </h2>
+    `);
     expect.assertions(3);
   });
-});
 
-describe('src/containers/pages/ANC', () => {
-  // any tests below this one, that's dependent on the selectors will break.
-  it('selectors are called with correct args', async () => {
-    const getClientsArrayMock = jest.spyOn(clientDux, 'getClientsArray');
-    fetch.once(JSON.stringify(allANC));
+  describe('src/containers/pages/ANC', () => {
+    // any tests below this one, that's dependent on the selectors will break.
+    it('selectors are called with correct args', async () => {
+      const getClientsMock = jest.spyOn(clientDux, 'getClientsArray');
+      fetch.once(JSON.stringify(allANC));
 
-    mount(<ConnectedANCListView {...props} />);
+      mount(
+        <Provider store={store}>
+          <Router history={history}>
+            <ConnectedANCListView {...props} />
+          </Router>
+        </Provider>
+      );
 
-    await new Promise(resolve => setImmediate(resolve));
+      await new Promise(resolve => setImmediate(resolve));
 
-    expect(getClientsArrayMock).toHaveBeenCalledWith({});
+      expect(getClientsMock).toHaveBeenCalledWith(SelectorState);
+    });
   });
 });
