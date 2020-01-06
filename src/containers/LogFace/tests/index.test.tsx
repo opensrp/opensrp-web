3d118d2c6331e4d8fcb14897a1ef1e992f078be3
FrankApiyo <franklineapiyo@gmail.com>
2020-01-06 12:11:27 +0300

    Rename test suit: components/ConnectedHeader --> containers/LogFace

--- src/containers/LogFace/tests/index.test.tsx
+++ src/containers/LogFace/tests/index.test.tsx
@@ -26,7 +26,7 @@ import { userLocations } from './userLocationFixtures';
 
 reducerRegistry.register(reducerName, reducer);
 
-describe('components/ConnectedHeader', () => {
+describe('containers/LogFace', () => {
   const props = { module: PREGNANCY };
   afterEach(() => {
     store.dispatch(removeSms);
