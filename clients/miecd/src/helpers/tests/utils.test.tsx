2a135037ad47a7a794ef281cea7cfdae7f6d0e02
FrankApiyo <franklineapiyo@gmail.com>
2020-01-08 13:06:19 +0300

    Fix tests

--- clients/miecd/src/helpers/tests/utils.test.tsx
+++ clients/miecd/src/helpers/tests/utils.test.tsx
@@ -16,9 +16,9 @@ import {
   NUTRITION,
   NUTRITION_COMPARTMENTS_URL,
   PREGNANCY,
-  PREGNANCY_COMPARTMENTS_URL,
-} from '../../constants';
-import { smsDataFixtures } from '../../containers/Compartments/test/fixtures';
+  PREGNANCY_COMPARTMENTS_URL
+} from "../../constants";
+import { smsDataFixtures } from "../../containers/Compartments/test/fixtures";
 import {
   communes,
   districts,
@@ -30,6 +30,12 @@ import { OpenSRPService } from '../../services/opensrp';
 import locationsReducer, { reducerName as locationsReducerName } from '../../store/ducks/locations';
 import { SmsData } from '../../store/ducks/sms_events';
 import smsReducer, { reducerName as smsReducerName } from '../../store/ducks/sms_events';
+import {
+  districts,
+  provinces,
+  villages
+} from "../../containers/HierarchichalDataTable/test/fixtures";
+import { SmsData } from "../../store/ducks/sms_events";
 import {
   buildHeaderBreadCrumb,
   convertMillisecondsToYear,
@@ -47,15 +53,16 @@ import {
   provinces,
   sortByEventDate,
   sortFunction,
-} from '../utils';
-import groupedSmsData from './fixtures';
+  getLinkToPatientDetail
+} from "../utils";
+import groupedSmsData from "./fixtures";
 
 reducerRegistry.register(smsReducerName, smsReducer);
 reducerRegistry.register(locationsReducerName, locationsReducer);
 
 jest.mock('@onaio/gatekeeper', () => ({
   getOnadataUserInfo: jest.fn(),
-  getOpenSRPUserInfo: jest.fn(),
+  getOpenSRPUserInfo: jest.fn()
 }));
 
 describe('helpers/utils/getLinkToPatientDetail', () => {
@@ -83,70 +90,75 @@ describe('helpers/utils', () => {
     jest.resetAllMocks();
   });
 
-  it('oAuthUserInfoGetter works for OpenSRP', () => {
-    const mock = jest.spyOn(gatekeeper, 'getOpenSRPUserInfo');
-    const resp = { foo: 'bar', oAuth2Data: { state: OPENSRP_OAUTH_STATE } };
+  it("oAuthUserInfoGetter works for OpenSRP", () => {
+    const mock = jest.spyOn(gatekeeper, "getOpenSRPUserInfo");
+    const resp = { foo: "bar", oAuth2Data: { state: OPENSRP_OAUTH_STATE } };
     oAuthUserInfoGetter(resp);
     expect(mock).toHaveBeenCalledWith(resp);
     mock.mockRestore();
   });
 
-  it('oAuthUserInfoGetter works for Ona', () => {
-    const mock = jest.spyOn(gatekeeper, 'getOnadataUserInfo');
-    const resp = { foo: 'bar', oAuth2Data: { state: ONADATA_OAUTH_STATE } };
+  it("oAuthUserInfoGetter works for Ona", () => {
+    const mock = jest.spyOn(gatekeeper, "getOnadataUserInfo");
+    const resp = { foo: "bar", oAuth2Data: { state: ONADATA_OAUTH_STATE } };
     oAuthUserInfoGetter(resp);
     expect(mock).toHaveBeenCalledWith(resp);
     mock.mockRestore();
   });
 
-  it('Ensure header should render works correctly', () => {
-    window.history.pushState({}, 'Page Title', '/login');
+  it("Ensure header should render works correctly", () => {
+    window.history.pushState({}, "Page Title", "/login");
     expect(headerShouldNotRender()).toBe(true);
   });
 
-  it('Ensure groupBy works correctly', () => {
-    expect(groupBy(smsDataFixtures.slice(0, 3), 'event_id')).toEqual(groupedSmsData);
+  it("Ensure groupBy works correctly", () => {
+    expect(groupBy(smsDataFixtures.slice(0, 3), "event_id")).toEqual(
+      groupedSmsData
+    );
   });
 });
 
-describe('helpers/utils/getLinkToPatientDetail', () => {
-  it('returns correct value when the client_type field is set to ec_woman', () => {
-    expect(getLinkToPatientDetail(smsDataFixtures[0], 'test')).toEqual(
-      'test/patient_detail/100NG6'
+describe("helpers/utils/getLinkToPatientDetail", () => {
+  it("returns correct value when the client_type field is set to ec_woman", () => {
+    expect(getLinkToPatientDetail(smsDataFixtures[0], "test")).toEqual(
+      "test/patient_detail/100NG6"
     );
   });
 
-  it('returns correct value when the client type field is set to ec_child', () => {
-    expect(getLinkToPatientDetail(smsDataFixtures[8], 'test')).toEqual(
-      'test/child_patient_detail/100P9K-20191001-01'
+  it("returns correct value when the client type field is set to ec_child", () => {
+    expect(getLinkToPatientDetail(smsDataFixtures[8], "test")).toEqual(
+      "test/child_patient_detail/100P9K-20191001-01"
     );
   });
 
-  it('returns correct value when the client_type field is set to ec_woman', () => {
+  it("returns correct value when the client_type field is set to ec_woman", () => {
     const smsDataItem = smsDataFixtures[0];
-    smsDataItem.client_type = 'NULL';
-    expect(getLinkToPatientDetail(smsDataFixtures[0], 'test')).toEqual('#');
-describe('getNumberSuffix', () => {
-  it('returns correct values for given inputs', () => {
-    expect(getNumberSuffix(1)).toEqual('st');
-    expect(getNumberSuffix(2)).toEqual('nd');
-    expect(getNumberSuffix(3)).toEqual('rd');
-    expect(getNumberSuffix(4)).toEqual('th');
+    smsDataItem.client_type = "NULL";
+    expect(getLinkToPatientDetail(smsDataFixtures[0], "test")).toEqual("#");
   });
 });
 
-describe('sortFuction', () => {
-  it('returns 0 when the sms event_id is the same', () => {
+describe("getNumberSuffix", () => {
+  it("returns correct values for given inputs", () => {
+    expect(getNumberSuffix(1)).toEqual("st");
+    expect(getNumberSuffix(2)).toEqual("nd");
+    expect(getNumberSuffix(3)).toEqual("rd");
+    expect(getNumberSuffix(4)).toEqual("th");
+  });
+});
+
+describe("sortFuction", () => {
+  it("returns 0 when the sms event_id is the same", () => {
     const smsData1: SmsData = JSON.parse(JSON.stringify(smsDataFixtures[0]));
     expect(sortFunction(smsData1, smsData1)).toEqual(0);
   });
-  it('returns 1 when the second sms event_id is larger than the first', () => {
+  it("returns 1 when the second sms event_id is larger than the first", () => {
     const smsData1: SmsData = JSON.parse(JSON.stringify(smsDataFixtures[0]));
     const smsData2: SmsData = JSON.parse(JSON.stringify(smsDataFixtures[1]));
     smsData2.event_id = `{Number(smsData1.event_id) + 3}`;
     expect(sortFunction(smsData1, smsData2)).toEqual(1);
   });
-  it('returns -1 when the first sms event_id is larger than the second', () => {
+  it("returns -1 when the first sms event_id is larger than the second", () => {
     const smsData1: SmsData = JSON.parse(JSON.stringify(smsDataFixtures[0]));
     const smsData2: SmsData = JSON.parse(JSON.stringify(smsDataFixtures[1]));
     smsData2.event_id = `{Number(smsData1.event_id) + 3}`;
@@ -154,102 +166,121 @@ describe('sortFuction', () => {
   });
 });
 
-describe('getModuleLink', () => {
-  it('returns the correct value given specific input', () => {
+describe("getModuleLink", () => {
+  it("returns the correct value given specific input", () => {
     expect(getModuleLink(PREGNANCY)).toEqual(PREGNANCY_COMPARTMENTS_URL);
     expect(getModuleLink(NUTRITION)).toEqual(NUTRITION_COMPARTMENTS_URL);
-    expect(getModuleLink(NBC_AND_PNC_CHILD)).toEqual(NBC_AND_PNC_COMPARTMENTS_URL);
-    expect(getModuleLink(NBC_AND_PNC_WOMAN)).toEqual(NBC_AND_PNC_COMPARTMENTS_URL);
+    expect(getModuleLink(NBC_AND_PNC_CHILD)).toEqual(
+      NBC_AND_PNC_COMPARTMENTS_URL
+    );
+    expect(getModuleLink(NBC_AND_PNC_WOMAN)).toEqual(
+      NBC_AND_PNC_COMPARTMENTS_URL
+    );
   });
 });
 
-describe('getLinkToHierarchichalDataTable', () => {
+describe("getLinkToHierarchichalDataTable", () => {
   expect(
-    getLinkToHierarchichalDataTable('low', PREGNANCY, 'test title', 0, 'location023423423423')
+    getLinkToHierarchichalDataTable(
+      "low",
+      PREGNANCY,
+      "test title",
+      0,
+      "location023423423423"
+    )
   ).toEqual(
-    '/pregnancy_compartments/hierarchicaldata/Pregnancy/low/test title/0/down/location023423423423/0'
+    "/pregnancy_compartments/hierarchicaldata/Pregnancy/low/test title/0/down/location023423423423/0"
   );
 });
 
-describe('buildHeaderBreadCrumb', () => {
-  it('works correctly if location id is a province', () => {
+describe("buildHeaderBreadCrumb", () => {
+  it("works correctly if location id is a province", () => {
     expect(
       buildHeaderBreadCrumb(
-        'd79168ed-ae95-43d1-8e45-9078212815f6',
+        "d79168ed-ae95-43d1-8e45-9078212815f6",
         provinces,
         districts,
         communes,
-        villages
+        villages,
+        "d1865325-11e6-4e39-817b-e676c1affecf"
       )
     ).toEqual({
-      level: 'Province',
-      location: 'Lang Son Province',
-      locationId: 'd79168ed-ae95-43d1-8e45-9078212815f6',
-      path: '',
+      level: "Province",
+      location: "Lang Son Province",
+      locationId: "d79168ed-ae95-43d1-8e45-9078212815f6",
+      path: ""
     });
   });
 
-  it('works correctly if location id is a district', () => {
+  it("works correctly if location id is a district", () => {
     expect(
       buildHeaderBreadCrumb(
-        '623b644d-a1f2-4c5e-b065-d60c0ae6501f',
+        "623b644d-a1f2-4c5e-b065-d60c0ae6501f",
         provinces,
         districts,
         communes,
-        villages
+        villages,
+        "d1865325-11e6-4e39-817b-e676c1affecf"
       )
     ).toEqual({
-      level: 'District',
-      location: 'Tay Ho District',
-      locationId: '623b644d-a1f2-4c5e-b065-d60c0ae6501f',
-      path: 'Hanoi / ',
+      level: "District",
+      location: "Tay Ho District",
+      locationId: "623b644d-a1f2-4c5e-b065-d60c0ae6501f",
+      path: "Hanoi / "
     });
   });
 
-  it('works correctly if location id is a commune', () => {
+  it("works correctly if location id is a commune", () => {
     expect(
       buildHeaderBreadCrumb(
-        '46d98781-7cc4-4c28-8379-a3552a57acfe',
+        "46d98781-7cc4-4c28-8379-a3552a57acfe",
         provinces,
         districts,
         communes,
-        villages
+        villages,
+        "d1865325-11e6-4e39-817b-e676c1affecf"
       )
     ).toEqual({
-      level: 'Commune',
-      location: 'Yen Phu Ward',
-      locationId: '46d98781-7cc4-4c28-8379-a3552a57acfe',
-      path: 'Hanoi / Tay Ho District / ',
+      level: "Commune",
+      location: "Yen Phu Ward",
+      locationId: "46d98781-7cc4-4c28-8379-a3552a57acfe",
+      path: "Hanoi / Tay Ho District / "
     });
   });
-  it('works correctly if location id is a village', () => {
+  it("works correctly if location id is a village", () => {
     expect(
       buildHeaderBreadCrumb(
-        'bb37165f-2ee9-4cad-a3b5-020c0cc75ccf',
+        "bb37165f-2ee9-4cad-a3b5-020c0cc75ccf",
         provinces,
         districts,
         communes,
-        villages
+        villages,
+        "d1865325-11e6-4e39-817b-e676c1affecf"
       )
     ).toEqual({
-      level: 'Village',
-      location: 'Yen Phu Village',
-      locationId: 'bb37165f-2ee9-4cad-a3b5-020c0cc75ccf',
-      path: 'Hanoi / Tay Ho District / Yen Phu Ward / ',
+      level: "Village",
+      location: "Yen Phu Village",
+      locationId: "bb37165f-2ee9-4cad-a3b5-020c0cc75ccf",
+      path: "Hanoi / Tay Ho District / Yen Phu Ward / "
     });
   });
 });
 
-describe('location data is available', () => {
-  expect(locationDataIsAvailable(villages, communes, districts, provinces)).toEqual(4);
+describe("location data is available", () => {
+  expect(
+    locationDataIsAvailable(villages, communes, districts, provinces)
+  ).toEqual(4);
   expect(locationDataIsAvailable([], [], [], [])).toEqual(0);
 });
 
-describe('getNumberOfDaysSinceDate', () => {
-  it('returns the correct number of days between 1/1/2020 and now', () => {
-    const dateString = '01/01/2020';
+describe("getNumberOfDaysSinceDate", () => {
+  it("returns the correct number of days between 1/1/2020 and now", () => {
+    const dateString = "01/01/2020";
     expect(getNumberOfDaysSinceDate(dateString)).toEqual(
-      Math.floor((new Date().getTime() - new Date(dateString).getTime()) / (1000 * 3600 * 24))
+      Math.floor(
+        (new Date().getTime() - new Date(dateString).getTime()) /
+          (1000 * 3600 * 24)
+      )
     );
   });
 });
