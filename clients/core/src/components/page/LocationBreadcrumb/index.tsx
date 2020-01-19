754bec064ca554b3d85ad26f4cb1372014a72f97
prodhan29 <02nahid02@gmail.com>
2020-01-19 16:40:03 +0600

    Add a dropdown where searched item will appear

--- clients/core/src/components/page/LocationBreadcrumb/index.tsx
+++ clients/core/src/components/page/LocationBreadcrumb/index.tsx
@@ -2,17 +2,55 @@ import React, { useState } from 'react';
 
 import './index.css';
 import { Location } from '../../../store/ducks/locations';
-import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem, ButtonDropdown, DropdownToggle, DropdownMenu } from 'reactstrap';
 import Select from 'react-select';
 
+export interface ComponentProps {
+    name?: string;
+    childNodes: any;
+    addBreadcrumItem(item: Location): any,
+}
+
+/* interface for locations  **/
 interface BreadcrumbState {
     breadcrumItems: any;
 }
+
+const buttonDropdownStyle = {
+    backgroundColor: 'white',
+    color: '#2297d6',
+    border: '0px',
+};
+
+/* probable searched items will appear as list **/
+const Dropdown = (props: ComponentProps) => {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const toggle = () => setDropdownOpen(prevState => !prevState);
+
+    return (
+        <ButtonDropdown isOpen={dropdownOpen} toggle={toggle}>
+            <DropdownToggle caret style={buttonDropdownStyle}>
+                {props.name}
+            </DropdownToggle>
+            <DropdownMenu style={{ width: '250px' }}>
+                <div style={{ paddingLeft: '8px' }}> Change option </div>
+                <SearchApp
+                    {...props}
+                    addBreadcrumItem={(item: any) => {
+                        toggle();
+                        props.addBreadcrumItem(item);
+                    }}
+                />
+            </DropdownMenu>
+        </ButtonDropdown>
+    );
+};
 
 const initialState: BreadcrumbState = {
     breadcrumItems: [],
 };
 
+/* Location breadcrumb. it will be used to filter data by locations **/
 class LocationBreadcrumb extends React.Component<{}, BreadcrumbState> {
     constructor(props: {}) {
         super(props);
