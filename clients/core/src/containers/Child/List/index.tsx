5d2002f7dd2b72422b7065b5e060f9abf18a3c83
prodhan29 <02nahid02@gmail.com>
2020-01-22 19:29:15 +0600

    Add the basic table for child-list

--- clients/core/src/containers/Child/List/index.tsx
+++ clients/core/src/containers/Child/List/index.tsx
@@ -1,13 +1,56 @@
 import reducerRegistry from '@onaio/redux-reducer-registry';
-import { some } from 'lodash';
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { Store } from 'redux';
+import { Link } from 'react-router-dom';
+import { Col, Row, Table } from 'reactstrap';
+import SearchBox from '../../../components/page/SearchBox';
 
 /** Display the client list  */
 class ClientList extends React.Component<{}, {}> {
     public render() {
-        return <div> child list </div>;
+        return (
+            <div>
+                <h3 className="household-title"> Child (408) </h3>
+                <Row>
+                    <Col md={5}>
+                        <div className="household-search-bar">
+                            <SearchBox searchCallBack={() => {}} placeholder={'Search Child'} />
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Table className="shadow-sm">
+                            <thead>
+                                <tr>
+                                    <th>HH ID Number</th>
+                                    <th>Family Name</th>
+                                    <th>Head of Household</th>
+                                    <th>Phone</th>
+                                    <th>Registered Date</th>
+                                    <th>Members</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Proshanto </td>
+                                    <td>head</td>
+                                    <td>0171211111</td>
+                                    <td>22-01-2020</td>
+                                    <td className="members-table-field">10</td>
+                                    <td>
+                                        <Link to={`/child/profile/`}>View</Link>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+            </div>
+        );
     }
 }
 
