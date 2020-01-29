b467391515b3f055a05d53e9b787303d5a3c4a30
prodhan29 <02nahid02@gmail.com>
2020-01-29 19:34:12 +0600

    ANC list is done

--- src/containers/ANC/List/index.tsx
+++ src/containers/ANC/List/index.tsx
@@ -2,7 +2,7 @@ import reducerRegistry from '@onaio/redux-reducer-registry';
 import { some } from 'lodash';
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Table, Row, Col } from 'reactstrap';
+import { Table, Row, Col, FormGroup, Input, Label } from 'reactstrap';
 import { Store } from 'redux';
 import Loading from '../../../components/page/Loading';
 import { OPENSRP_CLIENT_ENDPOINT, PAGINATION_SIZE } from '../../../configs/env';
@@ -36,14 +36,24 @@ class ANCList extends React.Component<ANCListProps, any> {
     this.state = {
       totalRecords: 0,
       ancArray: [],
+      startDate: '',
+      endDate: '',
+      loading: true,
     };
   }
 
-  public async componentDidMount() {
+  componentDidMount() {
+    this.getDataFromServer();
+  }
+
+  getDataFromServer = async (extraParam: any = {}) => {
     const params = {
+      ...extraParam,
       clientType: 'anc',
       pageNumber: '1',
       pageSize: PAGINATION_SIZE,
+      startDate: this.state.startDate,
+      endDate: this.state.endDate,
     };
     const { opensrpService } = this.props;
     const ancService = new opensrpService('client/searchByCriteria');
@@ -52,64 +62,123 @@ class ANCList extends React.Component<ANCListProps, any> {
     this.setState({
       totalRecords: response.total,
       ancArray: response.clients,
+      loading: false,
     });
-  }
+  };
+
+  searchFilter = (searchText: string) => {
+
+    this.setState(
+      {
+          ...this.state,
+        loading: true,
+      },
+      () => this.getDataFromServer({ searchText })
+    );
+  };
+
+  dateChange = (e: any) => {
+    this.setState(
+      {
+        ...this.state,
+        [e.target.name]: e.target.value,
+      },
+      () => {
+        if (this.state.startDate !== '' && this.state.endDate !== '') {
+          this.getDataFromServer();
+        }
+      }
+    );
+  };
 
   public render() {
     /** render loader if there are no clients in state */
 
-    if (!some(this.state.ancArray)) {
+    if (this.state.loading) {
       return <Loading />;
+    } else {
+      return (
+        <div>
+          <h3 className="household-title"> ANC ({this.state.totalRecords})</h3>
+          <Row>
+            <Col md={5}>
+              <div className="household-search-bar">
+                <SearchBox searchCallBack={this.searchFilter} placeholder={`Search Client`} />
+              </div>
+            </Col>
+            <Col md={2}>
+              <FormGroup style={{ marginTop: '-9px' }}>
+                <Label style={{ fontSize: '12px' }}> Start Date </Label>
+                <Input
+                  type="date"
+                  name="startDate"
+                  className="shadow-sm date-field"
+                  value={this.state.startDate}
+                  onChangeCapture={this.dateChange}
+                  placeholder="Start Date"
+                />
+              </FormGroup>
+            </Col>
+            <Col md={2}>
+              <FormGroup style={{ marginTop: '-9px' }}>
+                <Label style={{ fontSize: '12px' }}> End Date </Label>
+                <Input
+                  type="date"
+                  name="endDate"
+                  value={this.state.endDate}
+                  className="shadow-sm date-field"
+                  onChangeCapture={this.dateChange}
+                  placeholder="End Date"
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <LocationBreadcrumb />
+              <Table striped={true}>
+                <thead>
+                  <tr>
+                    <th>Identifier</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Location</th>
+                    <th>Age</th>
+                    <th>EDD</th>
+                    <th>Gestational age</th>
+                    <th>Last contact</th>
+                    <th>Risk category</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.ancArray.map((client: Client) => {
+                    return (
+                      <tr key={client.id}>
+                        <td>{client.id}</td>
+                        <td>{client.firstName}</td>
+                        <td>{client.lastName}</td>
+                        <td></td>
+                        <td>{client.attributes.dynamicProperties.age_year_part}</td>
+                        <td>{client.attributes.dynamicProperties.edd.split('T')[0]}</td>
+                        <td></td>
+                        <td>
+                          {client.attributes.dynamicProperties.last_contact_date.split('T')[0]}
+                        </td>
+                        <td></td>
+                        <td>
+                          <a href={`${'#'}`}> view </a>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+        </div>
+      );
     }
-    return (
-      <div>
-        <h3 className="household-title"> ANC ({this.state.totalRecords})</h3>
-        {/* <Row>
-          <Col md={5}>  
-            <div className="household-search-bar">
-              <SearchBox searchCallBack={() => {}} placeholder={`Search Client`} />
-            </div>
-          </Col>
-        </Row> */}
-        <LocationBreadcrumb />
-        <Table striped={true}>
-          <thead>
-            <tr>
-              <th>Identifier</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Location</th>
-              <th>Age</th>
-              <th>EDD</th>
-              <th>Gestational age</th>
-              <th>Last contact</th>
-              <th>Risk category</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.ancArray.map((client: Client) => {
-              return (
-                <tr key={client.id}>
-                  <td>{client.id}</td>
-                  <td>{client.firstName}</td>
-                  <td>{client.lastName}</td>
-                  <td></td>
-                  <td>{client.attributes.dynamicProperties.age_year_part}</td>
-                  <td></td>
-                  <td></td>
-                  <td>{client.attributes.dynamicProperties.last_contact_date.split('T')[0]}</td>
-                  <td></td>
-                  <td>
-                    <a href={`${'#'}`}> view </a>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
-      </div>
-    );
   }
 }
 
