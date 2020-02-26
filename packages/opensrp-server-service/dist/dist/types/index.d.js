4bc711f95aabfa976204d0c615225d31e54948c7
p-netm <pmuriuki98@gmail.com>
2020-02-26 18:22:23 +0300

    Transpile

---
+++ packages/opensrp-server-service/dist/dist/types/index.d.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _defineProperty2 = _interopRequireDefault(require("@babel/runtime/helpers/defineProperty"));
