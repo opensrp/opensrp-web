44358b79aa25ac018dde3bd2aea4ea401b506bef
p-netm <pmuriuki98@gmail.com>
2020-02-29 12:27:23 +0300

    Refactor types in src/index.tsx

--- packages/OpenSRPTable/src/index.tsx
+++ packages/OpenSRPTable/src/index.tsx
@@ -2,19 +2,30 @@
  * the react-table component
  */
 import * as React from 'react';
-import { Column, useSortBy, useTable, ColumnInstance, UseSortByColumnProps, Cell } from 'react-table';
+import {
+    Column,
+    useSortBy,
+    useTable,
+    ColumnInstance,
+    UseSortByColumnProps,
+    Cell,
+    UseTableHeaderGroupProps,
+    Row,
+} from 'react-table';
 import { PLEASE_DEFINE_SOME_COLUMNS } from './constants';
 import './index.css';
 
 /** props for the OpenSRPTable component */
 interface OpenSRPTableProps<T extends object> {
     /** a memoized list of columns  */
-    tableColumns: Column[];
+    tableColumns: Column<T>[];
     /** the data to be rendered in table */
     data: T[];
 }
 
-const defaultANCTableProps = {
+/** Default props for openSRPTable component */
+// Having trouble typing this to OpenSRPTableProps
+const defaultOpenSRPTableProps = {
     data: [],
     tableColumns: [
         {
@@ -25,10 +36,12 @@ const defaultANCTableProps = {
 };
 
 /** describe the column instance after including hooks. */
-interface ActualColumnInstance<D extends object = {}> extends ColumnInstance<D>, UseSortByColumnProps<D> {}
+interface ActualColumnInstance<D extends object> extends ColumnInstance<D>, UseSortByColumnProps<D> {}
 
 /** the component definition */
-function OpenSRPTable<T extends object>(props: OpenSRPTableProps<T> = defaultANCTableProps): ReturnType<React.FC<T>> {
+function OpenSRPTable<T extends object>(
+    props: OpenSRPTableProps<T> = defaultOpenSRPTableProps,
+): ReturnType<React.FC<T>> {
     const { tableColumns, data } = props;
 
     // Use the state and functions returned from useTable to build your UI
@@ -44,9 +57,9 @@ function OpenSRPTable<T extends object>(props: OpenSRPTableProps<T> = defaultANC
         <div>
             <table {...getTableProps()} className="table react-table">
                 <thead>
-                    {headerGroups.map((headerGroup, idx) => (
+                    {headerGroups.map((headerGroup: UseTableHeaderGroupProps<T>, idx: number) => (
                         <tr key={`thead-tr-${idx}`} {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map((c: ColumnInstance<T>, index) => {
+                            {headerGroup.headers.map((c: ColumnInstance<T>, index: number) => {
                                 const column = (c as unknown) as ActualColumnInstance<T>;
                                 <th key={`thead-th-${index}`} {...column.getHeaderProps(column.getSortByToggleProps())}>
                                     {column.render('Header')}
@@ -72,11 +85,11 @@ function OpenSRPTable<T extends object>(props: OpenSRPTableProps<T> = defaultANC
                     ))}
                 </thead>
                 <tbody {...getTableBodyProps()}>
-                    {rows.map((row, i) => {
+                    {rows.map((row: Row<T>, i: number) => {
                         prepareRow(row);
                         return (
                             <tr key={`tbody-tr-${i}`} {...row.getRowProps()}>
-                                {row.cells.map((cell: Cell, index: number) => {
+                                {row.cells.map((cell: Cell<T>, index: number) => {
                                     return (
                                         <td key={`tbody-td-${index}`} {...cell.getCellProps()}>
                                             {cell.render('Cell')}
