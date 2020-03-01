d226434e65ead2cafc8bd484f83932926de119e1
prodhan29 <02nahid02@gmail.com>
2020-03-01 16:15:07 +0600

    Add test cases

--- clients/core/src/containers/Child/List/tests/index.test.tsx
+++ clients/core/src/containers/Child/List/tests/index.test.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
-import { mount, shallow } from 'enzyme';
-import { ChildList } from '..';
+import { mount } from 'enzyme';
+import { ChildList, ChildListProps } from '..';
+import ConnectedChildList from '..';
+import * as fixtures from '../../../../store/ducks/tests/fixtures';
+import toJson from 'enzyme-to-json';
+import { Provider } from 'react-redux';
+import store from '../../../../store';
+import { fetchChildList } from '../../../../store/ducks/child';
 
-describe('containers/child/List/ChildList', () => {
+describe('containers/child/List', () => {
     beforeEach(() => {
         jest.resetAllMocks();
     });
@@ -10,5 +16,58 @@ describe('containers/child/List/ChildList', () => {
     it('renders without crashing', () => {
         const wrapper = mount(<ChildList />);
         expect(wrapper.length).toBe(1);
+    });
+
+    it('renders correctly', () => {
+        const mock: jest.Mock = jest.fn();
+        const opensrpServiceMock: jest.Mock = jest.fn();
+        const props = {
+            clientsArray: fixtures.clients,
+            fetchClientsActionCreator: mock,
+            location: mock,
+            match: mock,
+            opensrpService: opensrpServiceMock,
+        };
+        const wrapper = mount(<ChildList {...props} />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+        wrapper.unmount();
+    });
+
+    it('renders correctly when childArray is an empty array', () => {
+        const mock: jest.Mock = jest.fn();
+        const opensrpServiceMock: jest.Mock = jest.fn();
+        const props: ChildListProps = {
+            childArray: [],
+            fetchChild: mock,
+            opensrpService: opensrpServiceMock,
+            removeChild: mock,
+            totalRecords: 0,
+            setTotalRecords: mock,
+        };
+        const wrapper = mount(<ChildList {...props} />);
+        expect(toJson(wrapper.find('Ripple'))).toMatchSnapshot('Ripple Loader');
+        wrapper.unmount();
+    });
+
+    it('works correctly with the redux store', () => {
+        store.dispatch(fetchChildList(fixtures.childList));
+        const mock: jest.Mock = jest.fn();
+        const opensrpServiceMock: jest.Mock = jest.fn();
+        const props: ChildListProps = {
+            childArray: [],
+            fetchChild: mock,
+            opensrpService: opensrpServiceMock,
+            removeChild: mock,
+            totalRecords: 0,
+            setTotalRecords: mock,
+        };
+        const wrapper = mount(
+            <Provider store={store}>
+                <ConnectedChildList {...props} />
+                );
+            </Provider>,
+        );
+        expect(toJson(wrapper)).toMatchSnapshot();
+        wrapper.unmount();
     });
 });
