86d1decd4069d4e8257a4deb08c6d4114d3cbebe
p-netm <pmuriuki98@gmail.com>
2020-03-11 14:14:25 +0300

    Fix tsconfig and regenerate dist

--- packages/opensrp-server-service/dist/dist/types/index.d.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _defineProperty2 = _interopRequireDefault(require("@babel/runtime/helpers/defineProperty"));
