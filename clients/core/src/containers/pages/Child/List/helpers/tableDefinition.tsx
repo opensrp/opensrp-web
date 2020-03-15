ba33c43e4595d0c3abc64ab57d3655cecafcdf40
p-netm <pmuriuki98@gmail.com>
2020-03-15 17:52:43 +0300

    Add child register page table definition

---
+++ clients/core/src/containers/pages/Child/List/helpers/tableDefinition.tsx
@@ -0,0 +1,83 @@
+/* eslint-disable react/prop-types */
+import React, { useMemo } from 'react';
+import { Link } from 'react-router-dom';
+// Child specific configurable table columns
+export const useChildTableColumns = () => {
+    return useMemo(
+        () => [
+            {
+                /** placeholder for location table crumb */
+                // TODO - the location tableCrumb will be a render prop
+                // eslint-disable-next-line react/display-name
+                Header: () => (
+                    <>
+                        <nav aria-label="breadcrumb">
+                            <ol className="breadcrumb">
+                                <li className="breadcrumb-item">
+                                    <a href="#">Kenya</a>
+                                </li>
+                                <li className="breadcrumb-item">
+                                    <a href="#">Nairobi</a>
+                                </li>
+                                <li className="breadcrumb-item active" aria-current="page">
+                                    Kilimani
+                                </li>
+                            </ol>
+                        </nav>
+                    </>
+                ),
+                columns: [
+                    { Header: 'Identifier', accessor: 'baseEntityId', disableSortBy: true },
+                    {
+                        Header: 'First Name',
+                        accessor: 'firstName',
+                        disableSortBy: true,
+                    },
+                    {
+                        Header: 'Last Name',
+                        accessor: 'lastName',
+                        disableSortBy: true,
+                    },
+
+                    {
+                        Header: 'Location',
+                        accessor: '',
+                        disableSortBy: true,
+                    },
+                    {
+                        Header: 'Age',
+                        accessor: 'attributes.dynamicProperties.age_year_part',
+                    },
+                    {
+                        Header: 'Gender',
+                        accessor: 'gender',
+                    },
+                    {
+                        Header: 'Last Contact Date',
+                        accessor: 'attributes.dynamicProperties.last_contact_date',
+                    },
+
+                    {
+                        Header: 'Risk',
+                        accessor: '',
+                        disableSortBy: true,
+                    },
+                    {
+                        Header: 'Immunization status',
+                        accessor: '',
+                        disableSortBy: true,
+                    },
+                    {
+                        // eslint-disable-next-line react/display-name
+                        Cell: ({ row: { index } }) => <Link to="#">View</Link>,
+                        Header: 'Actions',
+                        accessor: '',
+                        disableSortBy: true,
+                    },
+                ],
+                id: 'tableCrumb',
+            },
+        ],
+        [],
+    );
+};
