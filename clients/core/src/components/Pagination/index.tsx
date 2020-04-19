efb96a02349e49367ec644c0ad85152f8559016d
p-netm <pmuriuki98@gmail.com>
2020-04-19 15:05:35 +0300

    Add temporary usePagination hook ,replaces @onaio/pagination

--- clients/core/src/components/Pagination/index.tsx
+++ clients/core/src/components/Pagination/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import * as React from 'react';
-import { PaginationOptions, usePagination } from '@onaio/pagination';
+import { PaginationOptions, usePagination } from './onaioPagination';
 import { fetchPageNumbers } from './utils';
 import { BootstrapJSX } from './JSX';
 
@@ -24,7 +24,7 @@ const Pagination: React.FC<Props> = props => {
     const { onPageChangeHandler, pageNeighbors, pageSize, totalRecords } = props;
 
     const initialPageSize = pageSize;
-    const options: PaginationOptions<{}> = {
+    const options: PaginationOptions = {
         pageSize: initialPageSize,
         totalRecords: totalRecords,
     };
@@ -49,10 +49,6 @@ const Pagination: React.FC<Props> = props => {
     React.useEffect(() => {
         onPageChangeHandler && onPageChangeHandler(paginationState.currentPage, paginationState.pageSize);
     }, [paginationState.currentPage]);
-
-    React.useEffect(() => {
-        goToPage(1);
-    }, [totalRecords]);
 
     return (
         <BootstrapJSX
