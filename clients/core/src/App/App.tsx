08dfcd436ea197300643d40debe6929754e61ae1
prodhan29 <02nahid02@gmail.com>
2020-04-21 13:35:29 +0600

    resolve merge conflicts

--- clients/core/src/App/App.tsx
+++ clients/core/src/App/App.tsx
@@ -72,7 +72,7 @@ class App extends Component {
                             <ConnectedPrivateRoute
                                 disableLoginProtection={DISABLE_LOGIN_PROTECTION}
                                 exact={true}
-                                path={HOUSEHOLD_PROFILE_URL}
+                                path={`${HOUSEHOLD_PROFILE_URL}/:id`}
                                 component={ConnectedHouseholdProfile}
                             />
 
