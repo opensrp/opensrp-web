7a8dffb069832fe274589ca5dec35229aa3005d1
prodhan29 <02nahid02@gmail.com>
2020-06-07 14:54:30 +0600

    Fix tableDefinition structure

--- clients/core/src/containers/Households/Profile/helpers/tableDefinition.tsx
+++ clients/core/src/containers/Households/Profile/helpers/tableDefinition.tsx
@@ -10,47 +10,38 @@ export const useMemberTableColumns = () => {
     return useMemo(
         () => [
             {
-                /** placeholder for location table crumb */
-                // TODO - the location tableCrumb will be a render prop
-                // eslint-disable-next-line react/display-name
-                Header: '',
-                columns: [
-                    {
-                        Header: 'First Name',
-                        accessor: 'firstName',
-                        disableSortBy: true,
-                    },
-                    {
-                        Header: 'Last Name',
-                        accessor: 'lastName',
-                        disableSortBy: true,
-                    },
-                    {
-                        Header: 'Age',
-                        accessor: 'attributes.dynamicProperties.age_year_part',
-                    },
+                Header: 'First Name',
+                accessor: 'firstName',
+                disableSortBy: true,
+            },
+            {
+                Header: 'Last Name',
+                accessor: 'lastName',
+                disableSortBy: true,
+            },
+            {
+                Header: 'Age',
+                accessor: 'attributes.dynamicProperties.age_year_part',
+            },
 
-                    {
-                        Header: 'Register Status',
-                        accessor: 'attributes.dynamicProperties.registration_status',
-                        disableSortBy: true,
-                    },
-                    {
-                        // eslint-disable-next-line react/display-name
-                        Cell: ({ row: { values } }: UseTableCellProps<Client>): ReturnType<React.FC> => (
-                            <>
-                                <Link to={`${values.baseEntityId}`}>{VIEW}</Link>{' '}
-                                <Link className="remove-action" to={`#`}>
-                                    Remove
-                                </Link>
-                            </>
-                        ),
-                        Header: 'Actions',
-                        accessor: '',
-                        disableSortBy: true,
-                    },
-                ],
-                id: 'tableCrumb',
+            {
+                Header: 'Register Status',
+                accessor: 'attributes.dynamicProperties.registration_status',
+                disableSortBy: true,
+            },
+            {
+                // eslint-disable-next-line react/display-name
+                Cell: ({ row: { values } }: UseTableCellProps<Client>): ReturnType<React.FC> => (
+                    <>
+                        <Link to={`${values.baseEntityId}`}>{VIEW}</Link>{' '}
+                        <Link className="remove-action" to={`#`}>
+                            Remove
+                        </Link>
+                    </>
+                ),
+                Header: 'Actions',
+                accessor: '',
+                disableSortBy: true,
             },
         ],
         [],
