aabb914252a654a8ce8bbebf464ef1168791f620
prodhan29 <02nahid02@gmail.com>
2020-06-09 20:51:09 +0600

    Add more test case to render team member table

--- clients/core/src/containers/pages/Teams/Profile/tests/index.test.tsx
+++ clients/core/src/containers/pages/Teams/Profile/tests/index.test.tsx
@@ -1,14 +1,25 @@
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { createLocation, createMemoryHistory } from 'history';
 import * as React from 'react';
-import { match } from 'react-router';
-import { TeamProfile } from '../index';
+import { match, MemoryRouter } from 'react-router';
+import ConnectedTeamProfile, { TeamProfile } from '../index';
 
-// import { Provider } from 'react-redux';
-// import store from '../../../../../store';
-import { removeTeamMembers, fetchTeamMembers } from '../../../../../store/ducks/teamMembers';
-import { fetchTeams } from '../../../../../store/ducks/teams';
+import { Provider } from 'react-redux';
+import store from '../../../../../store';
+import teamMember, {
+    removeTeamMembers,
+    fetchTeamMembers,
+    reducerName as teamMemberReducer,
+} from '../../../../../store/ducks/teamMembers';
 import * as fixtures from '../../../../../store/ducks/tests/fixtures';
+import reducerRegistry from '@onaio/redux-reducer-registry';
+import team, { fetchTeams, reducerName as teamsReducer } from '../../../../../store/ducks/teams';
+
+// /** register the team reducer */
+reducerRegistry.register(teamsReducer, team);
+
+/** register the teamMember reducer */
+reducerRegistry.register(teamMemberReducer, teamMember);
 
 const history = createMemoryHistory();
 const path = `/household-profile/:id/`;
@@ -62,6 +73,27 @@ describe('containers/households/Profile', () => {
         expect(wrapper.length).toBe(1);
     });
 
+    it('should render the member table', () => {
+        const props = {
+            members: fixtures.teamMemberList,
+            fetchTeam: fetchTeams,
+            fetchMembers: fetchTeamMembers,
+            removeMembers: removeTeamMembers,
+            opensrpService: classMock,
+            history,
+            team: fixtures.team1,
+            location,
+            match: matchVariable,
+        };
+        const component = mount(
+            <MemoryRouter initialEntries={['/']} keyLength={0}>
+                <TeamProfile {...props} />
+            </MemoryRouter>,
+        );
+        const wrapper = component.find('MemberTable');
+        expect(wrapper.length).toBe(1);
+    });
+
     it('should render when team is null', () => {
         const props = {
             members: [],
@@ -77,5 +109,34 @@ describe('containers/households/Profile', () => {
         const component = shallow(<TeamProfile {...props} />);
         const wrapper = component.find('#teamProfile');
         expect(wrapper.length).toBe(0);
+    });
+
+    it('should work with store', async () => {
+        store.dispatch(fetchTeams(fixtures.teamList));
+        store.dispatch(fetchTeamMembers(fixtures.teamMemberList));
+        const props = {
+            members: [],
+            fetchTeam: fetchTeams,
+            fetchMembers: fetchTeamMembers,
+            removeMembers: removeTeamMembers,
+            opensrpService: classMock,
+            history,
+            team: null,
+            location,
+            match: matchVariable,
+        };
+        const wrapper = mount(
+            <Provider store={store}>
+                <MemoryRouter initialEntries={['/']} keyLength={0}>
+                    <ConnectedTeamProfile {...props} />
+                </MemoryRouter>
+            </Provider>,
+        );
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        await new Promise((resolve: any) => setImmediate(resolve));
+        wrapper.update();
+        expect(wrapper).toMatchSnapshot();
+        wrapper.unmount();
     });
 });
