431f594d7d7bb29e5f4bafe7925bfff0959678f9
ciremusyoka <wambua.eric01@gmail.com>
2020-06-17 13:27:20 +0300

    Add reducer and actions for draft files

---
+++ clients/core/src/formConfig/ducks/manifestDraftFiles.ts
@@ -0,0 +1,29 @@
+import {
+    fetchActionCreatorFactory,
+    removeActionCreatorFactory,
+    reducerFactory,
+    getItemsByIdFactory,
+    getItemsArrayFactory,
+    getItemByIdFactory,
+} from '../../store/ducks/baseDux';
+import { ManifestFilesTypes } from './manifestFiles';
+
+/** reducer name */
+export const reducerName = 'manifestDraftFiles';
+
+/** manifest draft files Reducer */
+const reducer = reducerFactory<ManifestFilesTypes>(reducerName);
+
+// action
+/** fetch manifest draft files to store action */
+export const fetchManifestDraftFiles = fetchActionCreatorFactory<ManifestFilesTypes>(reducerName, 'id');
+
+/** clear manifest draft files data in store action*/
+export const removeManifestDraftFiles = removeActionCreatorFactory(reducerName);
+
+// selectors
+export const getAllManifestDraftFilesById = getItemsByIdFactory<ManifestFilesTypes>(reducerName);
+export const getManifestDraftFilesById = getItemByIdFactory<ManifestFilesTypes>(reducerName);
+export const getAllManifestDraftFilesArray = getItemsArrayFactory<ManifestFilesTypes>(reducerName);
+
+export default reducer;
