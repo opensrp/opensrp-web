5eda062503e164c239aa50e763ad417423a237dc
ciremusyoka <wambua.eric01@gmail.com>
2020-06-23 23:13:10 +0300

    Add upload file error tests

--- packages/opensrp-form-config/src/helpers/tests/services.test.ts
+++ packages/opensrp-form-config/src/helpers/tests/services.test.ts
@@ -3,17 +3,15 @@ import { OpenSRPServiceExtend, getToken } from '../services';
 /* eslint-disable-next-line @typescript-eslint/no-var-requires */
 const fetch = require('jest-fetch-mock');
 
+const baseUrl = 'https://test.smartregister.org/opensrp/rest/';
+
 describe('OpenSRPServiceExtend', () => {
     it('should get token', () => {
         expect(getToken(getFetchOptions)).toEqual('Bearer hunter2');
     });
 
     it('should post data', () => {
-        const service = new OpenSRPServiceExtend(
-            'https://test.smartregister.org/opensrp/rest/',
-            'files',
-            getFetchOptions,
-        );
+        const service = new OpenSRPServiceExtend(baseUrl, 'files', getFetchOptions);
         service.postData({ test: 'data' });
         expect(fetch.mock.calls).toEqual([
             [
@@ -29,5 +27,22 @@ describe('OpenSRPServiceExtend', () => {
                 },
             ],
         ]);
+    });
+
+    it('OpenSRPService attaches a non successful apiResponse correctly', async () => {
+        // json apiResponse object
+        const statusText = 'something happened';
+        fetch.mockResponseOnce(JSON.stringify('Some error happened'), { status: 500, statusText });
+        const service = new OpenSRPServiceExtend(baseUrl, 'form');
+        let error;
+        try {
+            await service.postData({ foo: 'bar' });
+        } catch (e) {
+            error = e;
+        }
+        expect(error).toEqual(new Error('OpenSRPService create on form failed, HTTP status 500'));
+        expect(error.description).toEqual('"Some error happened"');
+        expect(error.statusText).toEqual('something happened');
+        expect(error.statusCode).toEqual(500);
     });
 });
